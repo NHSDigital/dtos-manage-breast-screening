917e1676e58624943f19ba6a15dd99bbd079704b
Mat Moore <MatMoore@users.noreply.github.com>
2025-07-02 15:38:26 +0100

    refactor: split out last known mammogram presenter
    This needs to be reworked to use participant reported mammograms.
    It seems like a distinct enough thing to get its own presenter
    and separating it allows views to conditionally pass it into the
    participant details helper.

--- manage_breast_screening/mammograms/presenters.py
+++ manage_breast_screening/mammograms/presenters.py
@@ -24,9 +24,8 @@ def present_secondary_nav(pk):
 
 
 class AppointmentPresenter:
-    def __init__(self, appointment, last_known_screening=None):
+    def __init__(self, appointment):
         self._appointment = appointment
-        self._last_known_screening = last_known_screening
 
         self.allStatuses = AppointmentStatus
         self.pk = appointment.pk
@@ -56,8 +55,13 @@ class AppointmentPresenter:
             "is_confirmed": current_status.state == AppointmentStatus.CONFIRMED,
         }
 
+
+class LastKnownMammogramPresenter:
+    def __init__(self, last_known_screening):
+        self._last_known_screening = last_known_screening
+
     @cached_property
-    def last_known_screening(self):
+    def last_known_mammogram(self):
         return (
             {
                 "date": format_date(self._last_known_screening.created_at),
