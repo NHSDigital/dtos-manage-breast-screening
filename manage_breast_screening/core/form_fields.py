e951fef3bc161a225f6d6b01bd7b02da36ea7d86
Mat Moore <MatMoore@users.noreply.github.com>
2025-07-08 11:35:25 +0100

    Add a multi-value form field for dateInputs
    This custom form field provides compatability with the split date input
    from the design system. In the form we can deal with python date
    objects, but in the template we can access the values of the individual
    components.
    
    This is based on an archived form library from MoJ:
    https://github.com/ministryofjustice/django-govuk-forms
    
    Note that `MultiWidget` allows you to customise the names of the
    subwidgets, but it's not possible to do this in a way that is consistent
    with the generated IDs. The IDs will always be generated using their
    sequential order, e.g. foo_0, foo_1, foo_2. The dateInput component will
    derive the ID from the name if you don't specify
    params.items.id. To avoid mismatches, I've left the names as _0, _1, _2.

---
+++ manage_breast_screening/core/form_fields.py
@@ -0,0 +1,148 @@
+import datetime
+
+from django import forms
+from django.forms import ValidationError, widgets
+from django.utils.timezone import now
+from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy as _
+
+
+class YearField(forms.IntegerField):
+    """
+    In integer field that accepts years between 1900 and now
+    Allows 2-digit year entry which is converted depending on the `era_boundary`
+    Adapted from https://github.com/ministryofjustice/django-govuk-forms/blob/master/govuk_forms/fields.py
+    """
+
+    def __init__(self, era_boundary=None, **kwargs):
+        self.current_year = now().year
+        self.century = 100 * (self.current_year // 100)
+        if era_boundary is None:
+            # 2-digit dates are a minimum of 10 years ago by default
+            era_boundary = self.current_year - self.century - 10
+        self.era_boundary = era_boundary
+        bounds_error = gettext("Year should be between 1900 and %(current_year)s.") % {
+            "current_year": self.current_year
+        }
+        options = {
+            "min_value": 1900,
+            "max_value": self.current_year,
+            "error_messages": {
+                "min_value": bounds_error,
+                "max_value": bounds_error,
+                "invalid": gettext("Enter year as a number."),
+            },
+        }
+        options.update(kwargs)
+        super().__init__(**options)
+
+    def clean(self, value):
+        value = self.to_python(value)
+        if isinstance(value, int) and value < 100:
+            if value > self.era_boundary:
+                value += self.century - 100
+            else:
+                value += self.century
+        return super().clean(value)
+
+
+class SplitDateWidget(widgets.MultiWidget):
+    """
+    A widget that splits a date into 3 number inputs.
+    Adapted from https://github.com/ministryofjustice/django-govuk-forms/blob/master/govuk_forms/widgets.py
+    """
+
+    def __init__(self, attrs=None):
+        date_widgets = (
+            widgets.NumberInput(attrs=attrs),
+            widgets.NumberInput(attrs=attrs),
+            widgets.NumberInput(attrs=attrs),
+        )
+        super().__init__(date_widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return [value.day, value.month, value.year]
+        return [None, None, None]
+
+
+class SplitHiddenDateWidget(SplitDateWidget):
+    """
+    A widget that splits a date into 3 number inputs (hidden variant)
+    Adapted from https://github.com/ministryofjustice/django-govuk-forms/blob/master/govuk_forms/widgets.py
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for widget in self.widgets:
+            widget.input_type = "hidden"
+
+
+class SplitDateField(forms.MultiValueField):
+    """
+    A form field that can be rendered as 3 inputs using the dateInput component in the design system.
+    Adapted from https://github.com/ministryofjustice/django-govuk-forms/blob/master/govuk_forms/fields.py
+    """
+
+    widget = SplitDateWidget
+    hidden_widget = SplitHiddenDateWidget
+    default_error_messages = {"invalid": _("Enter a valid date.")}
+
+    def __init__(self, *args, **kwargs):
+        day_bounds_error = gettext("Day should be between 1 and 31.")
+        month_bounds_error = gettext("Month should be between 1 and 12.")
+
+        day_kwargs = {
+            "min_value": 1,
+            "max_value": 31,
+            "error_messages": {
+                "min_value": day_bounds_error,
+                "max_value": day_bounds_error,
+                "invalid": gettext("Enter day as a number."),
+            },
+        }
+        month_kwargs = {
+            "min_value": 1,
+            "max_value": 12,
+            "error_messages": {
+                "min_value": month_bounds_error,
+                "max_value": month_bounds_error,
+                "invalid": gettext("Enter month as a number."),
+            },
+        }
+        year_kwargs = {}
+
+        max_value = kwargs.pop("max_value", None)
+        if max_value:
+            day_kwargs["max_value"] = max_value.day
+            month_kwargs["max_value"] = max_value.month
+            year_kwargs["max_value"] = max_value.year
+
+        self.fields = [
+            forms.IntegerField(**day_kwargs),
+            forms.IntegerField(**month_kwargs),
+            YearField(**year_kwargs),
+        ]
+
+        super().__init__(self.fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        if data_list:
+            try:
+                if any(item in self.empty_values for item in data_list):
+                    raise ValueError
+                return datetime.date(data_list[2], data_list[1], data_list[0])
+            except ValueError:
+                raise ValidationError(self.error_messages["invalid"], code="invalid")
+        return None
+
+    def widget_attrs(self, widget):
+        attrs = super().widget_attrs(widget)
+        if not isinstance(widget, SplitDateWidget):
+            return attrs
+        for subfield, subwidget in zip(self.fields, widget.widgets):
+            if subfield.min_value is not None:
+                subwidget.attrs["min"] = subfield.min_value
+            if subfield.max_value is not None:
+                subwidget.attrs["max"] = subfield.max_value
+        return attrs
---
+++ manage_breast_screening/core/tests/test_form_fields.py
@@ -0,0 +1,98 @@
+import datetime
+
+import pytest
+from django.core.exceptions import ValidationError
+from django.forms import Form
+from pytest_django.asserts import assertHTMLEqual
+
+from ..form_fields import SplitDateField
+
+
+class TestSplitDateField:
+    def test_clean(self):
+        f = SplitDateField(max_value=datetime.date(2026, 12, 31))
+
+        assert f.clean([1, 12, 2025]) == datetime.date(2025, 12, 1)
+
+        with pytest.raises(ValidationError, match="This field is required."):
+            f.clean(None)
+
+        with pytest.raises(ValidationError, match="This field is required."):
+            f.clean("")
+
+        with pytest.raises(ValidationError, match="Enter a valid date."):
+            f.clean("hello")
+
+        with pytest.raises(
+            ValidationError,
+            match="['Enter day as a number.', 'Enter month as a number.', 'Enter year as a number.']",
+        ):
+            f.clean(["a", "b", "c"])
+
+        with pytest.raises(
+            ValidationError,
+            match="['Enter day as a number.', 'Enter month as a number.', 'Enter year as a number.']",
+        ):
+            f.clean(["", "", ""])
+
+        with pytest.raises(
+            ValidationError,
+            match="['Day should be between 1 and 31.', 'Month should be between 1 and 12.', 'Year should be between 1900 and 2025.']",
+        ):
+            f.clean([0, 13, 1800])
+
+    def test_has_changed(self):
+        f = SplitDateField(max_value=datetime.date(2026, 12, 31))
+        assert f.has_changed([1, 12, 2025], [2, 12, 2025])
+        assert f.has_changed([1, 12, 2025], [1, 11, 2025])
+        assert f.has_changed([1, 12, 2025], [1, 12, 2026])
+        assert not f.has_changed([1, 12, 2025], [1, 12, 2025])
+
+    def test_default_django_render(self):
+        class TestForm(Form):
+            date = SplitDateField(max_value=datetime.date(2026, 12, 31))
+
+        f = TestForm()
+
+        assertHTMLEqual(
+            str(f),
+            """
+            <div>
+                <fieldset>
+                    <legend>Date:</legend>
+                    <input type="number" name="date_0" min="1" max="31" required id="id_date_0">
+                    <input type="number" name="date_1" min="1" max="12" required id="id_date_1">
+                    <input type="number" name="date_2" min="1900" max="2026" required id="id_date_2">
+                </fieldset>
+            </div>
+            """,
+        )
+
+    def test_default_django_render_in_bound_form(self):
+        class TestForm(Form):
+            date = SplitDateField(max_value=datetime.date(2026, 12, 31))
+
+        f = TestForm({"date_0": "1", "date_1": "12", "date_2": "2025"})
+
+        assertHTMLEqual(
+            str(f),
+            """
+            <div>
+                <fieldset>
+                    <legend>Date:</legend>
+                    <input type="number" name="date_0" min="1" max="31" value="1" required id="id_date_0">
+                    <input type="number" name="date_1" min="1" max="12" value="12" required id="id_date_1">
+                    <input type="number" name="date_2" min="1900" max="2026" value="2025" required id="id_date_2">
+                </fieldset>
+            </div>
+            """,
+        )
+
+    def test_form_cleaned_data(self):
+        class TestForm(Form):
+            date = SplitDateField()
+
+        f = TestForm({"date_0": "1", "date_1": "12", "date_2": "2025"})
+
+        assert f.is_valid()
+        assert f.cleaned_data["date"] == datetime.date(2025, 12, 1)
