79c2440f4ac2be4a5c4e8e6f5789347dd057d0bb
Steve Laing <steve.laing@gmail.com>
2025-07-08 11:22:29 +0100

    Add ApiClient to make message batch requests
    This class handles JWT Bearer token generation and request body composition.

---
+++ manage_breast_screening/notifications/api_client.py
@@ -0,0 +1,85 @@
+import os
+import time
+import uuid
+
+import jwt
+import requests
+
+from manage_breast_screening.notifications.models import Message, MessageBatch
+
+EXPIRES_IN_MINUTES = 5
+
+
+class OAuthError(Exception):
+    pass
+
+
+class ApiClient:
+    def send_message_batch(self, message_batch: MessageBatch) -> requests.Response:
+        response = requests.post(
+            os.getenv("API_MESSAGE_BATCH_URL"),
+            headers=self.headers(),
+            json=self.message_batch_request_body(message_batch),
+            timeout=10,
+        )
+
+        return response
+
+    def message_batch_request_body(self, message_batch: MessageBatch) -> dict:
+        return {
+            "data": {
+                "type": "MessageBatch",
+                "attributes": {
+                    "routingPlanId": message_batch.routing_plan_id,
+                    "messageBatchReference": str(message_batch.id),
+                    "messages": [
+                        self.message_request_body(m)
+                        for m in message_batch.messages.all()
+                    ],
+                },
+            }
+        }
+
+    def message_request_body(self, message: Message) -> dict:
+        return {
+            "messageReference": str(message.id),
+            "recipient": {"nhsNumber": message.appointment.nhs_number},
+        }
+
+    def headers(self) -> dict:
+        return {
+            "content-type": "application/vnd.api+json",
+            "accept": "application/vnd.api+json",
+            "x-correlation-id": str(uuid.uuid4()),
+            "authorization": f"Bearer {self.bearer_token()}",
+        }
+
+    def bearer_token(self) -> str:
+        auth_jwt = jwt.encode(
+            {
+                "sub": os.getenv("OAUTH_API_KEY"),
+                "iss": os.getenv("OAUTH_API_KEY"),
+                "jti": str(uuid.uuid4()),
+                "aud": os.getenv("OAUTH_TOKEN_URL"),
+                "exp": int(time.time()) + (EXPIRES_IN_MINUTES * 60),
+            },
+            os.getenv("PRIVATE_KEY"),
+            "RS512",
+            {"alg": "RS512", "typ": "JWT", "kid": os.getenv("OAUTH_API_KID")},
+        )
+
+        response = requests.post(
+            os.getenv("OAUTH_TOKEN_URL"),
+            data={
+                "grant_type": "client_credentials",
+                "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+                "client_assertion": auth_jwt,
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            timeout=10,
+        )
+
+        if response.status_code != 200:
+            raise OAuthError(response.text)
+
+        return response.json()["access_token"]
