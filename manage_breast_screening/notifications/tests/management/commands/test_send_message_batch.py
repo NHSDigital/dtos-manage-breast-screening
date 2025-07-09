ff13e2293ecdb76bfa0a1845b769f85d853ba48c
Steve Laing <steve.laing@gmail.com>
2025-07-09 09:58:42 +0100

    Add Django admin command to send message batches
    This command finds Appointment records with a starts_at date within the
    4 week and 4 days window and batches them up into Messages within a
    MessageBatch and sends to the ApiClient.

---
+++ manage_breast_screening/notifications/tests/management/commands/test_send_message_batch.py
@@ -0,0 +1,115 @@
+import uuid
+from datetime import datetime, timedelta
+from unittest.mock import ANY, MagicMock, patch
+
+import pytest
+import requests
+
+from manage_breast_screening.notifications.api_client import ApiClient
+from manage_breast_screening.notifications.management.commands.send_message_batch import (
+    TZ_INFO,
+    Command,
+    CommandError,
+)
+from manage_breast_screening.notifications.models import Message, MessageBatch
+from manage_breast_screening.notifications.tests.factories import AppointmentFactory
+
+
+@patch.object(
+    ApiClient, "send_message_batch", return_value=MagicMock(spec=requests.Response)
+)
+class TestSendMessageBatch:
+    @pytest.fixture
+    def routing_plan_id(self):
+        return str(uuid.uuid4())
+
+    @pytest.mark.django_db
+    def test_handle_with_a_batch_to_send(
+        self, mock_send_message_batch, routing_plan_id
+    ):
+        """Test sending message batch with valid Appointment data"""
+        mock_send_message_batch.return_value.status_code = 201
+
+        appointment = AppointmentFactory(
+            starts_at=datetime.today().replace(tzinfo=TZ_INFO)
+        )
+
+        subject = Command()
+        mock_mark_batch_as_sent = MagicMock()
+        subject.mark_batch_as_sent = mock_mark_batch_as_sent
+
+        subject.handle(**{"routing_plan_id": routing_plan_id})
+
+        message_batches = MessageBatch.objects.filter(routing_plan_id=routing_plan_id)
+        assert message_batches.count() == 1
+        assert (
+            Message.objects.filter(
+                appointment=appointment, batch=message_batches[0]
+            ).count()
+            == 1
+        )
+        mock_mark_batch_as_sent.assert_called_once_with(message_batches[0], ANY)
+
+    @pytest.mark.django_db
+    def test_handle_with_nothing_to_send(
+        self, mock_send_message_batch, routing_plan_id
+    ):
+        """Test that no MessageBatch or Message records are created when no appointments need notifications"""
+        Command().handle(**{"routing_plan_id": routing_plan_id})
+
+        assert MessageBatch.objects.count() == 0
+        assert Message.objects.count() == 0
+
+    @pytest.mark.django_db
+    def test_handle_with_appointments_inside_schedule_window(
+        self, mock_send_message_batch, routing_plan_id
+    ):
+        """Test that appointments with date inside the schedule period are notified"""
+        mock_send_message_batch.return_value.status_code = 201
+        appointment = AppointmentFactory(
+            starts_at=datetime.now().replace(tzinfo=TZ_INFO)
+            + timedelta(weeks=4, days=4)
+        )
+
+        subject = Command()
+        mock_mark_batch_as_sent = MagicMock()
+        subject.mark_batch_as_sent = mock_mark_batch_as_sent
+
+        subject.handle(**{"routing_plan_id": routing_plan_id})
+
+        message_batches = MessageBatch.objects.filter(routing_plan_id=routing_plan_id)
+        assert message_batches.count() == 1
+        assert (
+            Message.objects.filter(
+                appointment=appointment, batch=message_batches[0]
+            ).count()
+            == 1
+        )
+        mock_mark_batch_as_sent.assert_called_once_with(message_batches[0], ANY)
+
+    @pytest.mark.django_db
+    def test_handle_with_failing_notifications(
+        self, mock_send_message_batch, routing_plan_id
+    ):
+        """Test that message batches which fail to send are marked correctly"""
+        mock_send_message_batch.return_value.status_code = 400
+        appointment = AppointmentFactory(
+            starts_at=datetime.now().replace(tzinfo=TZ_INFO)
+            + timedelta(weeks=4, days=4)
+        )
+
+        Command().handle(**{"routing_plan_id": routing_plan_id})
+
+        message_batches = MessageBatch.objects.filter(routing_plan_id=routing_plan_id)
+        assert message_batches.count() == 1
+        assert message_batches[0].status == "failed"
+        messages = Message.objects.filter(
+            appointment=appointment, batch=message_batches[0]
+        )
+        assert messages.count() == 1
+        assert messages[0].status == "failed"
+
+    def test_handle_with_error(self, mock_send_message_batch, routing_plan_id):
+        """Test that errors are caught and raised as CommandErrors"""
+        with pytest.raises(CommandError):
+            Command().handle()
