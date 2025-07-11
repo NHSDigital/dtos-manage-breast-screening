9ce6893af6d8650faddf8bb9e217e29275d37119
Josiel Souza <josiel.souza@nordcloud.com>
2025-07-11 17:53:06 +0100

    wip

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -100,7 +100,6 @@ module "webapp" {
   fetch_secrets_from_infra_key_vault = var.fetch_secrets_from_infra_key_vault
   infra_key_vault_name               = var.infra_key_vault_name
   infra_key_vault_rg                 = var.infra_key_vault_rg
-  infra_key_vault_id                 = data.azurerm_key_vault.infra.id
   enable_auth                        = var.enable_auth
   app_key_vault_id                   = module.app-key-vault.key_vault_id
   docker_image                       = var.docker_image
