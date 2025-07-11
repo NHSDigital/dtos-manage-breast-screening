f8a7562f277a9d018f712139d3261ca7e409e5bd
Josiel Souza <josiel.souza@nordcloud.com>
2025-07-11 17:48:22 +0100

    wip

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -89,7 +89,7 @@ module "db_migrate" {
 
 module "webapp" {
   providers = {
-    azurerm          = azurerm
+    azurerm     = azurerm
     azurerm.hub = azurerm.hub
   }
   source                             = "../modules/dtos-devops-templates/infrastructure/modules/container-app"
@@ -100,6 +100,7 @@ module "webapp" {
   fetch_secrets_from_infra_key_vault = var.fetch_secrets_from_infra_key_vault
   infra_key_vault_name               = var.infra_key_vault_name
   infra_key_vault_rg                 = var.infra_key_vault_rg
+  infra_key_vault_id                 = data.azurerm_key_vault.infra.id
   enable_auth                        = var.enable_auth
   app_key_vault_id                   = module.app-key-vault.key_vault_id
   docker_image                       = var.docker_image
