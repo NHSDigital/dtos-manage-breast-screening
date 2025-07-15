fb13a109016bd9440f7496b134d37a4f1d272af9
Steve Laing <steve.laing@gmail.com>
2025-07-15 11:10:17 +0100

    Add method to Helpers for uploading to blob storage

--- manage_breast_screening/notifications/tests/integration/helpers.py
+++ manage_breast_screening/notifications/tests/integration/helpers.py
@@ -1,6 +1,8 @@
-from azure.blob.storage import BlobServiceClient, ContainerClient
-from azure.core.exceptions import ResourceExistsError
 import os
+from typing import Any
+
+from azure.core.exceptions import ResourceExistsError
+from azure.storage.blob import BlobServiceClient, ContainerClient, ContentSettings
 
 
 class Helpers:
@@ -10,11 +12,29 @@ class Helpers:
         connection_string = os.getenv("BLOB_STORAGE_CONNECTION_STRING")
         container_name = os.getenv("BLOB_CONTAINER_NAME")
 
-        service_client = BlobServiceClient.from_connection_string(
-            connection_string
-        )
+        service_client = BlobServiceClient.from_connection_string(connection_string)
 
         try:
             return service_client.create_container(container_name)
         except ResourceExistsError:
             return service_client.get_container_client(container_name)
+
+    def add_to_blob_storage(
+        self,
+        filename: str,
+        content: str,
+        content_encoding="ASCII",
+        content_type="application/dat",
+    ) -> dict[str, Any]:
+        """Write a file to the configured blob container"""
+
+        container = self.find_or_create_blob_container()
+        blob_client = container.get_blob_client(filename)
+        return blob_client.upload_blob(
+            content,
+            blob_type="BlockBlob",
+            content_settings=ContentSettings(
+                content_type=content_type, content_encoding=content_encoding
+            ),
+            overwrite=True,
+        )
