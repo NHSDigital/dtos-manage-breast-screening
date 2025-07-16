8c037e1d96e2f9abe9d2da1e600f463de347cf18
Steve Laing <steve.laing@gmail.com>
2025-07-16 09:33:08 +0100

    Containerise dependencies for integration tests and add runner script

---
+++ manage_breast_screening/notifications/tests/integration/run.sh
@@ -0,0 +1,5 @@
+docker compose --profile integration run test-dependencies sleep 0.1
+poetry run pytest $(dirname "$(realpath $0)")
+test_exit_code=$?
+docker compose --profile integration down --volumes --remove-orphans
+exit $test_exit_code
