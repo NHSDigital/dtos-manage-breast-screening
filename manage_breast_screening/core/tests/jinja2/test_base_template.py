95ca2a1fd2bf62f2b5dbeab43009d806aef9f337
Mat Moore <MatMoore@users.noreply.github.com>
2025-07-21 09:08:10 +0100

    fix: use en-dash instead of hyphen

--- manage_breast_screening/core/tests/jinja2/test_base_template.py
+++ manage_breast_screening/core/tests/jinja2/test_base_template.py
@@ -12,10 +12,10 @@ form_with_errors.errors = {"field": ["mooo"]}
 @pytest.mark.parametrize(
     "page_title,form,expected_title",
     [
-        ("", None, "Manage breast screening - NHS"),
-        ("Page 1", None, "Page 1 - Manage breast screening - NHS"),
-        ("Page 2", form_with_errors, "Error: Page 2 - Manage breast screening - NHS"),
-        ("Page 3", form, "Page 3 - Manage breast screening - NHS"),
+        ("", None, "Manage breast screening – NHS"),
+        ("Page 1", None, "Page 1 – Manage breast screening – NHS"),
+        ("Page 2", form_with_errors, "Error: Page 2 – Manage breast screening – NHS"),
+        ("Page 3", form, "Page 3 – Manage breast screening – NHS"),
     ],
 )
 def test_page_title(jinja_env, page_title, form, expected_title):
