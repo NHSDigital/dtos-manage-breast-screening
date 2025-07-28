8365213fe215dac3c49110b9b77cb34bc9cb65c4
Mat Moore <MatMoore@users.noreply.github.com>
2025-07-28 11:54:56 +0100

    feat: add macro for updated special appointment banner

---
+++ manage_breast_screening/mammograms/tests/jinja2/test_special_appointment_banner.py
@@ -0,0 +1,108 @@
+from textwrap import dedent
+
+import pytest
+from pytest_django.asserts import assertHTMLEqual
+
+from manage_breast_screening.mammograms.presenters import SpecialAppointmentPresenter
+
+
+@pytest.fixture
+def presenter():
+    return SpecialAppointmentPresenter(
+        {
+            "PHYSICAL_RESTRICTION": {"details": "broken foot", "temporary": "True"},
+            "SOCIAL_EMOTIONAL_MENTAL_HEALTH": {},
+        },
+        "68d758d0-792d-430f-9c52-1e7a0c2aa1dd",
+    )
+
+
+def test_special_appointment_banner_with_change_link(jinja_env, presenter):
+    template = jinja_env.from_string(
+        dedent(
+            r"""
+                {% from 'mammograms/special_appointments/special_appointment_banner.jinja' import special_appointment_banner %}
+                {{ special_appointment_banner(presenter, show_change_link=True) }}
+                """
+        )
+    )
+
+    response = template.render({"presenter": presenter})
+
+    assertHTMLEqual(
+        response,
+        """
+        <div class="nhsuk-warning-callout">
+            <h3 class="nhsuk-warning-callout__label">    <span role="text">
+                <span class="nhsuk-u-visually-hidden">Important: </span>
+                Special appointment
+            </span></h3>
+
+            <dl class="nhsuk-summary-list nhsuk-summary-list">
+                <div class="nhsuk-summary-list__row">
+                    <dt class="nhsuk-summary-list__key">
+                    Physical restriction
+                    </dt>
+                    <dd class="nhsuk-summary-list__value">
+                    broken foot<br><span class="nhsuk-tag nhsuk-tag--white nhsuk-u-margin-top-2">Temporary</span>
+                    </dd>
+                </div>
+                <div class="nhsuk-summary-list__row">
+                    <dt class="nhsuk-summary-list__key">
+                    Social, emotional, and mental health
+                    </dt>
+                    <dd class="nhsuk-summary-list__value">
+                    <span style="color: var(--nhsuk-secondary-text-color);">No details provided</span>
+                    </dd>
+                </div>
+            </dl>
+            <div style="text-align: right;">
+                <a href="/mammograms/68d758d0-792d-430f-9c52-1e7a0c2aa1dd/special-appointment/" style="font-size: 19px;">Change<span class="nhsuk-u-visually-hidden"> special appointment requirements</span></a>
+            </div>
+        </div>
+    """,
+    )
+
+
+def test_special_appointment_banner_without_change_link(jinja_env, presenter):
+    template = jinja_env.from_string(
+        dedent(
+            r"""
+                {% from 'mammograms/special_appointments/special_appointment_banner.jinja' import special_appointment_banner %}
+                {{ special_appointment_banner(presenter, show_change_link=False) }}
+                """
+        )
+    )
+
+    response = template.render({"presenter": presenter})
+
+    assertHTMLEqual(
+        response,
+        """
+        <div class="nhsuk-warning-callout">
+            <h3 class="nhsuk-warning-callout__label">    <span role="text">
+                <span class="nhsuk-u-visually-hidden">Important: </span>
+                Special appointment
+            </span></h3>
+
+            <dl class="nhsuk-summary-list nhsuk-summary-list">
+                <div class="nhsuk-summary-list__row">
+                    <dt class="nhsuk-summary-list__key">
+                    Physical restriction
+                    </dt>
+                    <dd class="nhsuk-summary-list__value">
+                    broken foot<br><span class="nhsuk-tag nhsuk-tag--white nhsuk-u-margin-top-2">Temporary</span>
+                    </dd>
+                </div>
+                <div class="nhsuk-summary-list__row">
+                    <dt class="nhsuk-summary-list__key">
+                    Social, emotional, and mental health
+                    </dt>
+                    <dd class="nhsuk-summary-list__value">
+                    <span style="color: var(--nhsuk-secondary-text-color);">No details provided</span>
+                    </dd>
+                </div>
+            </dl>
+        </div>
+    """,
+    )
