42c9e79f2f0a45527eb6aef3552c73e905d4810a
Alastair Lock <alastair.lock@gmail.com>
2025-08-01 16:48:27 +0100

    wip

--- infrastructure/modules/container-apps/main.tf
+++ infrastructure/modules/container-apps/main.tf
@@ -47,7 +47,7 @@ module "webapp" {
   docker_image                     = var.docker_image
   user_assigned_identity_ids       = [module.db_connect_identity.id]
   environment_variables = {
-    ALLOWED_HOSTS   = "manage-breast-screening-web-${var.environment}.${var.default_domain}"
+    ALLOWED_HOSTS   = "${var.app_short_name}-web-${var.environment}.${var.default_domain}"
     DATABASE_HOST   = module.postgres.host
     DATABASE_NAME   = module.postgres.database_names[0]
     DATABASE_USER   = module.db_connect_identity.name
