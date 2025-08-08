99548bfab1746d1635ac2ca8bcda7b3799884cc2
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-08-08 11:51:04 +0100

    Update dev and review environments

---
+++ infrastructure/environments/review/variables.sh
@@ -0,0 +1,6 @@
+ENV_CONFIG=review
+AZURE_SUBSCRIPTION="Digital Screening DToS - Core Services Dev"
+HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
+HUB=dev
+TERRAFORM_MODULES_REF=main
+ENABLE_SOFT_DELETE=false
