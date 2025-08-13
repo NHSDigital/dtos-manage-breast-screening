31bd4e690f0f1020065fa31fcac1d0cc0be8d793
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-08-13 14:32:23 +0100

    Make resource_group_init script more verbose
    Also ask for confirmation to execute the first bicep

--- scripts/bash/resource_group_init.sh
+++ scripts/bash/resource_group_init.sh
@@ -10,17 +10,27 @@ STORAGE_ACCOUNT_NAME="$6"
 APP_SHORT_NAME="$7"
 ARM_SUBSCRIPTION_ID="$8"
 
-echo Deploy to the hub subscription...
+echo "Deploy to hub subscription $HUB_SUBSCRIPTION_ID..."
+az deployment sub create --location "$REGION" --template-file infrastructure/terraform/resource_group_init/main.bicep \
+  --subscription "$HUB_SUBSCRIPTION_ID" \
+  --parameters enableSoftDelete="$ENABLE_SOFT_DELETE" envConfig="$ENV_CONFIG" region="$REGION" \
+    storageAccountRGName="$STORAGE_ACCOUNT_RG" storageAccountName="$STORAGE_ACCOUNT_NAME" appShortName="$APP_SHORT_NAME" --what-if
+
+read -r -p "Are you sure you want to execute the deployment? (y/n): " confirm
+[[ "$confirm" != "y" ]] && exit 0
+
 output=$(az deployment sub create --location "$REGION" --template-file infrastructure/terraform/resource_group_init/main.bicep \
   --subscription "$HUB_SUBSCRIPTION_ID" \
   --parameters enableSoftDelete="$ENABLE_SOFT_DELETE" envConfig="$ENV_CONFIG" region="$REGION" \
     storageAccountRGName="$STORAGE_ACCOUNT_RG" storageAccountName="$STORAGE_ACCOUNT_NAME" appShortName="$APP_SHORT_NAME")
 
+echo "$output"
+
 echo Capture the outputs...
 miName=$(echo "$output" | jq -r '.properties.outputs.miName.value')
 miPrincipalID=$(echo "$output" | jq -r '.properties.outputs.miPrincipalID.value')
 
-echo Deploy to the core subscription...
+echo "Deploy to core subscription $ARM_SUBSCRIPTION_ID..."
 az deployment sub create --location "$REGION" --template-file infrastructure/terraform/resource_group_init/core.bicep \
   --subscription "$ARM_SUBSCRIPTION_ID" \
   --parameters miName="$miName" miPrincipalId="$miPrincipalID" --confirm-with-what-if
