42a976f5939af56eb1cf1e94bc32be231052054c
Cameron Hargreaves <cameron.hargreaves1@nhs.net>
2025-08-14 15:21:41 +0100

    wip

--- manage_breast_screening/notifications/management/commands/send_failed_message_batch.py
+++ manage_breast_screening/notifications/management/commands/send_failed_message_batch.py
@@ -1,3 +1,8 @@
+import json
+import os
+import time
+from logging import getLogger
+
 from django.core.management.base import BaseCommand, CommandError
 
 from manage_breast_screening.notifications.api_client import ApiClient
@@ -5,6 +10,9 @@ from manage_breast_screening.notifications.management.commands.command_helpers i
     MessageBatchHelpers,
 )
 from manage_breast_screening.notifications.models import MessageBatch
+from manage_breast_screening.notifications.services.queue import Queue
+
+logger = getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -13,31 +21,49 @@ class Command(BaseCommand):
     a failed status and retries sending it to the Communications API.
     """
 
-    def add_arguments(self, parser):
-        parser.add_argument("message_batch_id")
-
     def handle(self, *args, **options):
         # Look for the MessageBatch and check validity
-        batch_id = options["message_batch_id"]
+
+        queue = Queue.RetryMessageBatches()
+        failedMessageBatch = queue.get(1)
+
+        print(failedMessageBatch)
+
+        if failedMessageBatch is None:
+            logger.info("No batches in queue")
+            return
+
+        queue.delete(failedMessageBatch)
+
         message_batch = MessageBatch.objects.filter(
-            id=batch_id, status="failed"
+            id=failedMessageBatch["message_batch_id"], status="failed_recoverable"
         ).first()
         if message_batch is None:
             raise CommandError(
-                f"Message Batch with id {batch_id} and status of 'failed' not found"
+                f"Message Batch with id {failedMessageBatch['message_batch_id']} and status of 'failed' not found"
             )
 
         # Try to send the MessageBatch
-        try:
-            response = ApiClient().send_message_batch(message_batch)
-            if response.status_code == 201:
-                MessageBatchHelpers.mark_batch_as_sent(
-                    message_batch=message_batch, response_json=response.json()
-                )
-            else:
-                raise CommandError(
-                    f"Message Batch with id {batch_id} not sent: {response.status_code}, {response.reason}"
-                )
+        if failedMessageBatch["retry_count"] < int(os.getenv("RETRY_LIMIT", 0)):
+            time.sleep(os.getenv("RETRY_DELAY") * failedMessageBatch["retry_count"])
 
-        except Exception as e:
-            raise CommandError(e)
+            try:
+                response = ApiClient().send_message_batch(message_batch)
+                if response.status_code == 201:
+                    MessageBatchHelpers.mark_batch_as_sent(
+                        message_batch=message_batch, response_json=response.json()
+                    )
+                    raise CommandError(
+                        f"Message Batch with id {failedMessageBatch['message_batch_id']} not sent: {response.status_code}, {response.reason}"
+                    )
+
+            except Exception as e:
+                raise CommandError(e)
+                queue.add(
+                    json.dumps(
+                        {
+                            "message_batch_id": str(message_batch.id),
+                            "retry_count": failedMessageBatch["retry_count"] + 1,
+                        }
+                    )
+                )
--- manage_breast_screening/notifications/tests/management/commands/test_send_failed_message_batch.py
+++ manage_breast_screening/notifications/tests/management/commands/test_send_failed_message_batch.py
@@ -1,9 +1,6 @@
-import uuid
-from unittest.mock import ANY, MagicMock, patch
+from unittest.mock import MagicMock, patch
 
-import pytest
 import requests
-from django.core.management.base import CommandError
 
 from manage_breast_screening.notifications.api_client import ApiClient
 from manage_breast_screening.notifications.management.commands.command_helpers import (
@@ -12,72 +9,85 @@ from manage_breast_screening.notifications.management.commands.command_helpers i
 from manage_breast_screening.notifications.management.commands.send_failed_message_batch import (
     Command,
 )
-from manage_breast_screening.notifications.tests.factories import MessageBatchFactory
+from manage_breast_screening.notifications.services.queue import Queue
 
 
 @patch.object(
     ApiClient, "send_message_batch", return_value=MagicMock(spec=requests.Response)
 )
+@patch.object(Queue, "RetryMessageBatches", return_value=MagicMock(spec=Queue))
 @patch.object(MessageBatchHelpers, "mark_batch_as_sent")
 class TestSendFailedMessageBatch:
-    @pytest.mark.django_db
-    def test_handle_batch_not_found(
-        self, mock_mark_batch_as_sent, mock_send_message_batch
+    # @pytest.mark.django_db
+    # def test_handle_batch_not_found(
+    #     self, mock_mark_batch_as_sent, mock_retry_message_batches, mock_send_message_batch
+    # ):
+    #     subject = Command()
+    #     batch_id = uuid.uuid4()
+
+    #     with pytest.raises(CommandError) as error:
+    #         subject.handle()
+
+    #     assert (
+    #         str(error.value)
+    #         == f"Message Batch with id {batch_id} and status of 'failed' not found"
+    #     )
+
+    # @pytest.mark.django_db
+    # def test_not_failed_yet(self, mock_mark_batch_as_sent, mock_retry_message_batches, mock_senmock_retry_message_batchesd_message_batch):
+    #     subject = Command()
+    #     batch_id = uuid.uuid4()
+    #     _scheduled_batch = MessageBatchFactory(id=batch_id)
+
+    #     with pytest.raises(CommandError) as error:
+    #         subject.handle()
+
+    #     assert (
+    #         str(error.value)
+    #         == f"Message Batch with id {batch_id} and status of 'failed' not found"
+    #     )
+
+    # @pytest.mark.django_db
+    # def test_server_error_when_resending_batch(
+    #     self, mock_mark_batch_as_sent, mock_retry_message_batches, mock_send_message_batch, monkeypatch
+    # ):
+    #     mock_send_message_batch.return_value.status_code = 500
+    #     mock_send_message_batch.return_value.reason = "Request timed out"
+    #     subject = Command()
+    #     batch_id = uuid.uuid4()
+    #     _failed_batch = MessageBatchFactory(id=batch_id, status="failed")
+
+    #     with pytest.raises(CommandError) as error:
+    #         subject.handle()
+
+    #     assert (
+    #         str(error.value)
+    #         == f"Message Batch with id {batch_id} not sent: 500, Request timed out"
+    #     )
+
+    # @pytest.mark.django_db
+    # def test_successfully_resends_batch(
+    #     self, mock_mark_batch_as_sent, mock_retry_message_batches, mock_send_message_batch, monkeypatch
+    # ):
+    #     mock_send_message_batch.return_value.status_code = 201
+    #     subject = Command()
+    #     batch_id = uuid.uuid4()
+    #     failed_batch = MessageBatchFactory(id=batch_id, status="failed")
+
+    #     subject.handle()
+
+    #     mock_mark_batch_as_sent.assert_called_once_with(
+    #         message_batch=failed_batch, response_json=ANY
+    #     )
+
+    def test_no_batches_in_queue(
+        self,
+        mock_mark_batch_as_sent,
+        mock_retry_message_batches,
+        mock_send_message_batch,
     ):
         subject = Command()
-        batch_id = uuid.uuid4()
 
-        with pytest.raises(CommandError) as error:
-            subject.handle(**{"message_batch_id": batch_id})
+        subject.handle()
 
-        assert (
-            str(error.value)
-            == f"Message Batch with id {batch_id} and status of 'failed' not found"
-        )
-
-    @pytest.mark.django_db
-    def test_not_failed_yet(self, mock_mark_batch_as_sent, mock_send_message_batch):
-        subject = Command()
-        batch_id = uuid.uuid4()
-        _scheduled_batch = MessageBatchFactory(id=batch_id)
-
-        with pytest.raises(CommandError) as error:
-            subject.handle(**{"message_batch_id": batch_id})
-
-        assert (
-            str(error.value)
-            == f"Message Batch with id {batch_id} and status of 'failed' not found"
-        )
-
-    @pytest.mark.django_db
-    def test_server_error_when_resending_batch(
-        self, mock_mark_batch_as_sent, mock_send_message_batch, monkeypatch
-    ):
-        mock_send_message_batch.return_value.status_code = 500
-        mock_send_message_batch.return_value.reason = "Request timed out"
-        subject = Command()
-        batch_id = uuid.uuid4()
-        _failed_batch = MessageBatchFactory(id=batch_id, status="failed")
-
-        with pytest.raises(CommandError) as error:
-            subject.handle(**{"message_batch_id": batch_id})
-
-        assert (
-            str(error.value)
-            == f"Message Batch with id {batch_id} not sent: 500, Request timed out"
-        )
-
-    @pytest.mark.django_db
-    def test_successfully_resends_batch(
-        self, mock_mark_batch_as_sent, mock_send_message_batch, monkeypatch
-    ):
-        mock_send_message_batch.return_value.status_code = 201
-        subject = Command()
-        batch_id = uuid.uuid4()
-        failed_batch = MessageBatchFactory(id=batch_id, status="failed")
-
-        subject.handle(**{"message_batch_id": batch_id})
-
-        mock_mark_batch_as_sent.assert_called_once_with(
-            message_batch=failed_batch, response_json=ANY
-        )
+        assert mock_retry_message_batches.delete.call_count == 0
