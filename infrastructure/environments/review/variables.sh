2888f53e074345900ca653094345b816528ef810
Alastair Lock <alastair.lock@gmail.com>
2025-08-21 11:34:41 +0100

    DTOSS-10668: Amend postgresql-flexible Terraform module to parameterise prevent_destroy lifecycle

--- infrastructure/environments/review/variables.sh
+++ infrastructure/environments/review/variables.sh
@@ -2,5 +2,5 @@ ENV_CONFIG=review
 AZURE_SUBSCRIPTION="Digital Screening DToS - Core Services Dev"
 HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
 HUB=dev
-TERRAFORM_MODULES_REF=main
+TERRAFORM_MODULES_REF=feat/DTOSS-10668-Amend-postgresql-flexible-Terraform-module-to-parameterise-prevent-destroy-lifecycle
 ENABLE_SOFT_DELETE=false
