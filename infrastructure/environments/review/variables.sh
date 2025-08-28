b33af9892ff0aa4a5c934586fdbc1ca313f77d8a
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-08-28 14:06:15 +0100

    test

--- infrastructure/environments/review/variables.sh
+++ infrastructure/environments/review/variables.sh
@@ -2,5 +2,5 @@ ENV_CONFIG=review
 AZURE_SUBSCRIPTION="Digital Screening DToS - Core Services Dev"
 HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
 HUB=dev
-TERRAFORM_MODULES_REF=main
+TERRAFORM_MODULES_REF=fix/DTOSS-9703-update-diagnostic-settings-module-pt1
 ENABLE_SOFT_DELETE=false
