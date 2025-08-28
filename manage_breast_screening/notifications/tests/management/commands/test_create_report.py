1a7b4bf2bd50bcb3964006a0527747a26fcaad7d
Steve Laing <steve.laing@gmail.com>
2025-08-28 11:59:31 +0100

    Add Django command to create reports
    This command can generate and store both aggregate and failures reports.

---
+++ manage_breast_screening/notifications/tests/management/commands/test_create_report.py
@@ -0,0 +1,93 @@
+from contextlib import contextmanager
+from datetime import datetime
+from unittest.mock import MagicMock, patch
+
+import pytest
+from django.core.management.base import CommandError
+
+from manage_breast_screening.notifications.management.commands.create_report import (
+    Command,
+)
+
+
+class TestCreateReports:
+    @pytest.fixture(autouse=True)
+    def setup(self, monkeypatch):
+        monkeypatch.setenv("REPORTS_CONTAINER_NAME", "reports")
+
+    @pytest.fixture
+    def csv_data(self):
+        return "this,that,1,8,1,2,1"
+
+    @pytest.fixture
+    def now(self):
+        return datetime.today()
+
+    @contextmanager
+    def mocked_dependencies(self, dataframe, csv_data, now):
+        module = (
+            "manage_breast_screening.notifications.management.commands.create_report"
+        )
+
+        with patch(f"{module}.BlobStorage") as mock_storage:
+            mock_blob_storage = MagicMock()
+            mock_storage.return_value = mock_blob_storage
+
+            with patch(f"{module}.pandas.read_sql") as mock_read_sql:
+                mock_read_sql.return_value = dataframe
+
+                with patch(f"{module}.datetime") as mock_datetime:
+                    mock_datetime.today.return_value = now
+
+                    yield (mock_blob_storage,)
+
+    @pytest.fixture
+    def dataframe(self, csv_data):
+        df = MagicMock()
+        df.to_csv.return_value = csv_data
+        return df
+
+    def test_handle_creates_aggregate_report(self, dataframe, csv_data, now):
+        with self.mocked_dependencies(dataframe, csv_data, now) as md:
+            Command().handle(**{"report_type": "aggregate"})
+
+        mock_blob_storage = md[0]
+        mock_blob_storage.add.assert_called_once_with(
+            now.strftime("%Y-%m-%dT%H:%M:%S-aggregate-report.csv"),
+            csv_data,
+            content_type="text/csv",
+            container_name="reports",
+        )
+
+    def test_handle_creates_failures_report(self, dataframe, csv_data, now):
+        with self.mocked_dependencies(dataframe, csv_data, now) as md:
+            Command().handle(**{"report_type": "failures"})
+
+        mock_blob_storage = md[0]
+        mock_blob_storage.add.assert_called_once_with(
+            now.strftime("%Y-%m-%dT%H:%M:%S-failures-report.csv"),
+            csv_data,
+            content_type="text/csv",
+            container_name="reports",
+        )
+
+    def test_handle_defaults_to_failures_report(self, dataframe, csv_data, now):
+        with self.mocked_dependencies(dataframe, csv_data, now) as md:
+            Command().handle()
+
+        mock_blob_storage = md[0]
+        mock_blob_storage.add.assert_called_once_with(
+            now.strftime("%Y-%m-%dT%H:%M:%S-failures-report.csv"),
+            csv_data,
+            content_type="text/csv",
+            container_name="reports",
+        )
+
+    def test_handle_raises_command_error(self):
+        with patch(
+            "manage_breast_screening.notifications.queries.failures_query.FailuresQuery"
+        ) as mock_query:
+            mock_query.sql.side_effect = Exception("err")
+
+            with pytest.raises(CommandError):
+                Command().handle()
