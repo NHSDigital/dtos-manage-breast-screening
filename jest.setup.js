3a332d116fb2fd6887877379c5a5d9acfae80741
Colin Rotherham <work@colinr.com>
2025-09-02 13:40:00 +0100

    Configure jsdom environment for NHS.UK frontend support checks

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom'
+import { outdent } from 'outdent'
 
 /**
  * Mock fetch() function for jsdom
@@ -7,4 +8,23 @@ Object.defineProperty(window, 'fetch', {
   configurable: true,
   writable: true,
   value: jest.fn().mockResolvedValue(undefined)
+})
+
+beforeEach(() => {
+  const stylesheet = document.createElement('style')
+
+  stylesheet.innerHTML = outdent`
+    :root {
+      --nhsuk-breakpoint-mobile: 20rem;
+      --nhsuk-breakpoint-tablet: 40.0625rem;
+      --nhsuk-breakpoint-desktop: 48.0625rem;
+      --nhsuk-breakpoint-large-desktop: 61.875rem;
+    }
+  `
+
+  // Add styles for NHS.UK frontend checks
+  document.head.appendChild(stylesheet)
+
+  // Flag NHS.UK frontend as supported
+  document.body.classList.add('nhsuk-frontend-supported')
 })
