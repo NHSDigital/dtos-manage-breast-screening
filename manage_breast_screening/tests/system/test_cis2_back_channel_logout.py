ca829c80cb98fb2a75021151c9e8d7fa6c9b264d
Malcolm Baig <git@baig.org>
2025-09-04 15:03:33 +0100

    Fix bug in CIS2 back channel system spec
    Our CIS2 OAuth client is cached in the registry of a running Django app
    from the first time the get_cis2_client function is called. This
    includes caching of the payload received from CIS2's JWKS endpoint.
    
    In our system test, this means we have to ensure that there is
    consistency between:
    - the key pairs we generate
    - the mocked response of the JWKS URI
    - the cached value of the JWKS endpoint in the OAuth client
    
    This isn't currently happening. As a result, our 'expired token'
    scenario is passing but for the wrong reasons. It fails on a bad
    signature error rather than an expiry error.
    
    To correct this we:
    
    - Move all key pair set up and request mocking into a pytest fixture
      at the top of the system test (rather than inline) so it's clearer
      what's being mocked
    - Make use of yield in this fixture so we can easily specify some
      clean up
    - As part of this clean up, remove the 'cis2' OAuth client from the
      registry
    
    This results in a new client being set up for each of these test
    scenarios.

--- manage_breast_screening/tests/system/test_cis2_back_channel_logout.py
+++ manage_breast_screening/tests/system/test_cis2_back_channel_logout.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 import requests_mock
 from authlib.jose import JsonWebKey, jwt
 from django.conf import settings
@@ -7,6 +8,8 @@ from django.contrib.auth import get_user_model
 from django.test.client import Client as TestClient
 from django.urls import reverse
 from playwright.sync_api import expect
+
+from manage_breast_screening.auth.oauth import oauth
 
 from .system_test_setup import SystemTestCase
 
@@ -18,18 +21,47 @@ class TestCIS2BackChannelLogout(SystemTestCase):
     and mocks calls to the CIS2 server metadata and JWKS endpoints.
     """
 
+    @pytest.fixture(autouse=True)
+    def around_each(self):
+        # 1. Generate a key pair for the CIS2 auth server
+        self.cis2_key_id = "key1"
+        self.cis2_jwk = JsonWebKey.generate_key(
+            "RSA", 2048, is_private=True, options={"kid": self.cis2_key_id}
+        )
+
+        # 2. Mock the CIS2 auth server metadata
+        self._requests_mocker = requests_mock.Mocker()
+        self._requests_mocker.start()
+        self.issuer = "test-issuer"
+        jwks_url = "http://example.com/test/oidc/jwks"
+        self._requests_mocker.get(
+            settings.CIS2_SERVER_METADATA_URL,
+            json={"issuer": self.issuer, "jwks_uri": jwks_url},
+            status_code=200,
+        )
+
+        # 3. Mock the CIS2 auth server JWKS endpoint
+        public_jwk = self.cis2_jwk.as_dict(is_private=False)
+        self._requests_mocker.get(
+            jwks_url,
+            json={"keys": [public_jwk]},
+            status_code=200,
+        )
+
+        yield
+
+        self._requests_mocker.stop()
+        # Remove the cached oauth client from the registry to avoid leaking state between tests
+        oauth._clients.pop("cis2", None)
+
     def test_back_channel_logout_invalidates_user_sessions(self):
         self.given_i_am_signed_in()
-        self.and_cis2_has_a_key_pair()
-        self.and_the_cis2_jwks_endpoint_is_setup()
         self.and_there_is_a_cis2_logout_token()
         self.when_the_back_channel_logout_endpoint_is_called()
         self.then_i_am_logged_out()
 
     def test_back_channel_logout_with_expired_token_is_rejected(self):
         self.given_i_am_signed_in()
-        self.and_cis2_has_a_key_pair()
-        self.and_the_cis2_jwks_endpoint_is_setup()
         self.and_there_is_an_expired_cis2_logout_token()
         self.when_the_back_channel_logout_endpoint_is_called()
         self.then_the_request_is_rejected_and_i_remain_logged_in()
@@ -43,33 +75,6 @@ class TestCIS2BackChannelLogout(SystemTestCase):
         self.page.goto(self.live_server_url + reverse("clinics:index"))
         header = self.page.get_by_role("navigation")
         expect(header.get_by_text("Log out")).to_be_visible()
-
-    def and_cis2_has_a_key_pair(self):
-        self.cis2_key_id = "key1"
-        self.cis2_jwk = JsonWebKey.generate_key(
-            "RSA", 2048, is_private=True, options={"kid": self.cis2_key_id}
-        )
-
-    def and_the_cis2_jwks_endpoint_is_setup(self):
-        self._requests_mocker = requests_mock.Mocker()
-        self._requests_mocker.start()
-        self.addCleanup(self._requests_mocker.stop)
-
-        metadata_url = settings.CIS2_SERVER_METADATA_URL
-        self.issuer = "test-issuer"
-        jwks_url = "http://example.com/test/oidc/jwks"
-        self._requests_mocker.get(
-            metadata_url,
-            json={"issuer": self.issuer, "jwks_uri": jwks_url},
-            status_code=200,
-        )
-
-        public_jwk = self.cis2_jwk.as_dict(is_private=False)
-        self._requests_mocker.get(
-            jwks_url,
-            json={"keys": [public_jwk]},
-            status_code=200,
-        )
 
     def and_there_is_a_cis2_logout_token(self):
         self.token = self._create_logout_token()
