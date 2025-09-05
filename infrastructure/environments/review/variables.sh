1542f6f235846b015237ab2460a0c0ec12e4f276
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-09-05 10:55:12 +0100

    TEMP

--- infrastructure/environments/review/variables.sh
+++ infrastructure/environments/review/variables.sh
@@ -2,5 +2,5 @@ ENV_CONFIG=review
 AZURE_SUBSCRIPTION="Digital Screening DToS - Core Services Dev"
 HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
 HUB=dev
-TERRAFORM_MODULES_REF=main
+TERRAFORM_MODULES_REF=DTOS-10841-pg-container
 ENABLE_SOFT_DELETE=false
