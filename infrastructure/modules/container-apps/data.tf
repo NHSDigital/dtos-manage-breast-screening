4bc5db85b5a099a4f059a1d31d70475fc03121a6
Josiel Souza <josiel.souza@nordcloud.com>
2025-09-09 18:16:19 +0100

    [DTOSS-10792] - Add private dns for pep

--- infrastructure/modules/container-apps/data.tf
+++ infrastructure/modules/container-apps/data.tf
@@ -3,3 +3,17 @@ data "azurerm_client_config" "current" {}
 data "azuread_group" "postgres_sql_admin_group" {
   display_name = var.postgres_sql_admin_group
 }
+
+data "azurerm_private_dns_zone" "storage-account-blob" {
+  provider = azurerm.hub
+
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = "rg-hub-${var.hub}-uks-private-dns-zones"
+}
+
+data "azurerm_private_dns_zone" "storage-account-queue" {
+  provider = azurerm.hub
+
+  name                = "privatelink.queue.core.windows.net"
+  resource_group_name = "rg-hub-${var.hub}-uks-private-dns-zones"
+}
