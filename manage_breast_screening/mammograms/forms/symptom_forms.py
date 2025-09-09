0d34efe450bb8f7b9d5592cb0c2c63a446a50910
Mat Moore <MatMoore@users.noreply.github.com>
2025-09-09 17:01:41 +0100

    wip symptom form

--- manage_breast_screening/mammograms/forms/symptom_forms.py
+++ manage_breast_screening/mammograms/forms/symptom_forms.py
@@ -9,40 +9,52 @@ from manage_breast_screening.core.form_fields import (
     ChoiceField,
     SplitDateField,
 )
+from manage_breast_screening.forms.fields import yes_no_field
 
 
 # TODO: delegate values to model changes
-class RightOrLeftChoices(TextChoices):
+class RightLeftOtherChoices(TextChoices):
     RIGHT = "RIGHT", "Right breast"
     LEFT = "LEFT", "Left breast"
     OTHER = "OTHER", "Other"
+
+
+class HowLongChoices(TextChoices):
+    LESS_THAN_THREE_MONTHS = "LESS_THAN_THREE_MONTHS", "Less than 3 months"
+    THREE_MONTHS_TO_A_YEAR = "THREE MONTHS TO A YEAR", "3 months to a year"
+    ONE_TO_THREE_YEARS = "ONE_TO_THREE_YEARS", "1 to 3 years"
+    OVER_THREE_YEARS = "OVER_THREE_YEARS", "Over 3 years"
+    SINCE_A_SPECIFIC_DATE = "SINCE_A_SPECIFIC_DATE", "Since a specific date"
+    NOT_SURE = "NOT_SURE", "Not sure"
 
 
 # TODO: create mixin for conditional field validation
 
 
 class LumpForm(Form):
-    where_located = ChoiceField(choices=RightOrLeftChoices)
-    other_area_description = CharField(required=False)
-    how_long = ChoiceField()
-    specific_date = SplitDateField()  # TODO rebase and pick up the include_day param
-    intermittent = BooleanField(required=False)
-    recently_resolved = BooleanField(required=False)
-    when_resolved = CharField(required=False)
-    investigated = ChoiceField()
-    additional_info = CharField(required=False)
-
-    def __init__(self, instance=None):
-        pass
-
-
-class SwellingForm(Form):
-    where_located = ChoiceField(choices=RightOrLeftChoices)
-    how_long = ChoiceField()
-    intermittent = BooleanField(required=False)
-    recently_resolved = BooleanField(required=False)
-    investigated = ChoiceField()
-    additional_info = CharField(required=False)
+    where_located = ChoiceField(
+        choices=RightLeftOtherChoices, label="Where is the lump located?"
+    )
+    other_area_description = CharField(
+        required=False,
+        label="Describe the specific area",
+        hint="For example, the left armpit",
+    )
+    how_long = ChoiceField(
+        choices=HowLongChoices, label="How long has this symptom existed?"
+    )
+    specific_date = SplitDateField(
+        hint="For example, 3 2025"
+    )  # TODO rebase and pick up the include_day param
+    intermittent = BooleanField(required=False, label="The symptom is intermittent")
+    recently_resolved = BooleanField(
+        required=False, label="The symptom has recently resolved"
+    )
+    when_resolved = CharField(
+        required=False, label="Describe when", hint="For example, 3 days ago"
+    )
+    investigated = yes_no_field(label="Has this been investigated?")
+    additional_info = CharField(required=False, label="Additional info (optional)")
 
     def __init__(self, instance=None):
         pass
