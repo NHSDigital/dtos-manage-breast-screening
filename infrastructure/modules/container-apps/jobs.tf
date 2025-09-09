bd6c4421d2ac75b42529a987c4a3c240f23a8668
Steve Laing <steve.laing@gmail.com>
2025-09-09 11:52:15 +0100

    Move db_setup job to jobs.tf
    Improve the definition of environment variables for other jobs.
    Expose the MI client IDs in the environment as these will be needed for connectivity to storage.

--- infrastructure/modules/container-apps/jobs.tf
+++ infrastructure/modules/container-apps/jobs.tf
@@ -32,7 +32,7 @@ locals {
       job_short_name     = "smb"
       job_container_args = "send_message_batch"
     }
-    # Retries messahe batches which failed to send via NHS Notify.
+    # Retries message batches which failed to send via NHS Notify.
     # Executes every 30 minutes.
     retry_failed_message_batch = {
       cron_expression = "*/30 * * * *"
@@ -77,6 +77,37 @@ locals {
   }
 }
 
+# populate the database
+module "db_setup" {
+  source = "../dtos-devops-templates/infrastructure/modules/container-app-job"
+
+  name                         = "${var.app_short_name}-dbm-${var.environment}"
+  container_app_environment_id = var.container_app_environment_id
+  resource_group_name          = azurerm_resource_group.main.name
+
+  # Run everything through /bin/sh
+  container_command = ["/bin/sh", "-c"]
+
+  container_args = [
+    var.seed_demo_data
+    ? "python manage.py migrate && python manage.py seed_demo_data --noinput"
+    : "python manage.py migrate"
+  ]
+  secret_variables = var.deploy_database_as_container ? { DATABASE_PASSWORD = resource.random_password.admin_password[0].result } : {}
+  docker_image     = var.docker_image
+  # user_assigned_identity_ids = var.deploy_database_as_container ? [] : [module.db_connect_identity[0].id]
+  user_assigned_identity_ids = flatten([
+    [module.azure_blob_storage_identity.id],
+    [module.azure_queue_storage_identity.id],
+    var.deploy_database_as_container ? [] : [module.db_connect_identity[0].id]
+  ])
+  environment_variables = merge(
+    local.common_env,
+    var.deploy_database_as_container ? local.container_db_env : local.azure_db_env
+  )
+
+}
+
 module "scheduled_jobs" {
   source   = "../dtos-devops-templates/infrastructure/modules/container-app-job"
   for_each = local.scheduled_jobs
@@ -94,8 +125,12 @@ module "scheduled_jobs" {
     "python manage.py ${each.value.job_container_args}"
   ]
 
-  docker_image               = var.docker_image
-  user_assigned_identity_ids = [module.azure_blob_storage_identity.id, module.azure_queue_storage_identity.id, module.db_connect_identity.id]
+  docker_image = var.docker_image
+  user_assigned_identity_ids = flatten([
+    [module.azure_blob_storage_identity.id],
+    [module.azure_queue_storage_identity.id],
+    var.deploy_database_as_container ? [] : [module.db_connect_identity[0].id]
+  ])
 
   environment_variables = merge(
     local.common_env,
