bac9a4b7be8876c002623235c46cec35f5e2a7da
Mat Moore <MatMoore@users.noreply.github.com>
2025-09-11 12:12:57 +0100

    failing system test

--- manage_breast_screening/tests/system/clinical/test_recording_symptoms.py
+++ manage_breast_screening/tests/system/clinical/test_recording_symptoms.py
@@ -18,12 +18,12 @@ class TestRecordingSymptoms(SystemTestCase):
         self.when_i_click_on_add_a_lump()
         self.then_i_see_the_add_a_lump_form()
 
-        # self.when_i_select_right_breast()
-        # self.and_i_select_less_than_three_months()
-        # self.and_i_select_no_the_symptom_has_not_been_investigated()
-        # self.and_i_click_submit()
-        # self.then_i_am_back_on_the_medical_information_page()
-        # self.and_the_lump_on_the_right_breast_is_listed()
+        self.when_i_select_right_breast()
+        self.and_i_select_less_than_three_months()
+        self.and_i_select_no_the_symptom_has_not_been_investigated()
+        self.and_i_click_save_symptom()
+        self.then_i_am_back_on_the_medical_information_page()
+        self.and_the_lump_on_the_right_breast_is_listed()
 
     def test_editing_and_removing_symptoms(self):
         # self.given_i_am_logged_in_as_a_clinical_user()
@@ -66,3 +66,25 @@ class TestRecordingSymptoms(SystemTestCase):
 
     def then_i_see_the_add_a_lump_form(self):
         expect(self.page.get_by_text("Details of the lump")).to_be_visible()
+
+    def when_i_select_right_breast(self):
+        self.page.get_by_label("Right breast").click()
+
+    def and_i_select_less_than_three_months(self):
+        self.page.get_by_label("Less than 3 months").click()
+
+    def and_i_select_no_the_symptom_has_not_been_investigated(self):
+        legend = self.page.get_by_text("Has this been investigated?")
+        fieldset = self.page.get_by_role("group").filter(has=legend)
+        fieldset.get_by_label("No").click()
+
+    def and_i_click_save_symptom(self):
+        self.page.get_by_text("Save symptom").click()
+
+    def then_i_am_back_on_the_medical_information_page(self):
+        self.expect_url("mammograms:record_medical_information", pk=self.appointment.pk)
+
+    def and_the_lump_on_the_right_breast_is_listed(self):
+        expect(
+            self.page.locator(".nhsuk-summary-list__row", has_text="Lump")
+        ).to_contain_text("Right breast")
