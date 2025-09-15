6f3d410e1728798d6c15bf0af3a72e692b615697
Alastair Lock <alastair.lock@gmail.com>
2025-09-15 15:21:46 +0100

    Updating some variables

--- infrastructure/environments/pre-production/variables.sh
+++ infrastructure/environments/pre-production/variables.sh
@@ -2,6 +2,6 @@ ENV_CONFIG=pre-production
 ENVIRONMENT=pre-production
 AZURE_SUBSCRIPTION="Digital Screening DToS - Core Services PreProd"
 HUB_SUBSCRIPTION="Digital Screening DToS - Core Services Prod Hub"
-HUB=
+HUB=prod
 TERRAFORM_MODULES_REF=main
 ENABLE_SOFT_DELETE=false
