63bb6f3aeb2b91af6b3193b9264246f761a7199f
Steve Laing <steve.laing@gmail.com>
2025-09-18 15:40:50 +0100

    Use BlobStorage to obtain container client

--- manage_breast_screening/notifications/tests/integration/test_store_mesh_messages.py
+++ manage_breast_screening/notifications/tests/integration/test_store_mesh_messages.py
@@ -1,14 +1,13 @@
 import os
 from datetime import datetime
-from functools import cached_property
 
 import pytest
-from azure.storage.blob import BlobServiceClient, ContainerClient
 from mesh_client import MeshClient
 
 from manage_breast_screening.notifications.management.commands.store_mesh_messages import (
     Command,
 )
+from manage_breast_screening.notifications.services.blob_storage import BlobStorage
 from manage_breast_screening.notifications.tests.integration.helpers import Helpers
 
 
@@ -38,6 +37,9 @@ class TestStoreMeshMessages:
         test_file_2 = helpers.get_test_file_path("ABC_20241202091321_APPT_107.dat")
         helpers.add_file_to_mesh_mailbox(test_file_1)
         helpers.add_file_to_mesh_mailbox(test_file_2)
+        container_client = BlobStorage().find_or_create_container(
+            os.getenv("BLOB_CONTAINER_NAME")
+        )
 
         with MeshClient(
             url=os.getenv("NBSS_MESH_HOST"),
@@ -51,8 +53,8 @@ class TestStoreMeshMessages:
 
             subject.handle()
 
-            for blob in self.container_client.list_blobs():
-                blob_client = self.container_client.get_blob_client(blob.name)
+            for blob in container_client.list_blobs():
+                blob_client = container_client.get_blob_client(blob.name)
                 blob_content = blob_client.download_blob(
                     max_concurrency=1, encoding="ASCII"
                 ).readall()
@@ -66,15 +68,6 @@ class TestStoreMeshMessages:
                 with open(file_path) as test_file:
                     assert test_file.read() == blob_content
 
-                self.container_client.delete_blob(blob.name)
+                container_client.delete_blob(blob.name)
 
             assert len(client.list_messages()) == 0
-
-    @cached_property
-    def container_client(self) -> ContainerClient:
-        connection_string = os.getenv("BLOB_STORAGE_CONNECTION_STRING")
-        container_name = os.getenv("BLOB_CONTAINER_NAME")
-
-        return BlobServiceClient.from_connection_string(
-            connection_string
-        ).get_container_client(container_name)
