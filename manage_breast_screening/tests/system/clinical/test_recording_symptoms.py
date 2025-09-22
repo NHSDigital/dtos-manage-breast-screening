688f2f1483f912ebb65bb8366501b9c8d1ee17b2
Mat Moore <MatMoore@users.noreply.github.com>
2025-09-22 12:20:30 +0100

    system test for changing a symptom

--- manage_breast_screening/tests/system/clinical/test_recording_symptoms.py
+++ manage_breast_screening/tests/system/clinical/test_recording_symptoms.py
@@ -2,11 +2,15 @@ import pytest
 from django.urls import reverse
 from playwright.sync_api import expect
 
-from manage_breast_screening.participants.models.symptom import SymptomType
+from manage_breast_screening.participants.models.symptom import (
+    RelativeDateChoices,
+    SymptomType,
+)
 from manage_breast_screening.participants.tests.factories import (
     AppointmentFactory,
     ParticipantFactory,
     ScreeningEpisodeFactory,
+    SymptomFactory,
 )
 
 from ..system_test_setup import SystemTestCase
@@ -34,11 +38,35 @@ class TestRecordingSymptoms(SystemTestCase):
         self.then_i_am_back_on_the_medical_information_page()
         self.and_the_lump_on_the_right_breast_is_listed()
 
+    def test_changing_a_symptom(self):
+        self.given_i_am_logged_in_as_a_clinical_user()
+        self.and_there_is_an_appointment_with_a_symptom_added_in_the_last_three_months()
+        self.and_i_am_on_the_record_medical_information_page()
+        self.when_i_click_on_change()
+        self.then_less_than_three_months_should_be_selected()
+
+        self.when_i_select_three_months_to_a_year()
+        self.and_i_click_save_symptom()
+
+        self.then_i_am_back_on_the_medical_information_page()
+        self.and_i_see_three_months_to_a_year()
+
     def and_there_is_an_appointment(self):
         self.participant = ParticipantFactory(first_name="Janet", last_name="Williams")
         self.screening_episode = ScreeningEpisodeFactory(participant=self.participant)
         self.appointment = AppointmentFactory(screening_episode=self.screening_episode)
         self.provider = self.appointment.provider
+
+    def and_there_is_an_appointment_with_a_symptom_added_in_the_last_three_months(self):
+        self.participant = ParticipantFactory(first_name="Janet", last_name="Williams")
+        self.screening_episode = ScreeningEpisodeFactory(participant=self.participant)
+        self.appointment = AppointmentFactory(screening_episode=self.screening_episode)
+        self.provider = self.appointment.provider
+        self.symptom = SymptomFactory(
+            appointment=self.appointment,
+            lump=True,
+            when_started=RelativeDateChoices.LESS_THAN_THREE_MONTHS,
+        )
 
     def and_i_am_on_the_record_medical_information_page(self):
         self.page.goto(
@@ -61,6 +89,9 @@ class TestRecordingSymptoms(SystemTestCase):
     def and_i_select_less_than_three_months(self):
         self.page.get_by_label("Less than 3 months").click()
 
+    def when_i_select_three_months_to_a_year(self):
+        self.page.get_by_label("3 months to a year").click()
+
     def and_i_select_no_the_symptom_has_not_been_investigated(self):
         legend = self.page.get_by_text("Has this been investigated?")
         fieldset = self.page.get_by_role("group").filter(has=legend)
@@ -78,3 +109,20 @@ class TestRecordingSymptoms(SystemTestCase):
         )
         row = self.page.locator(".nhsuk-summary-list__row").filter(has=key)
         expect(row).to_contain_text("Right breast")
+
+    def when_i_click_on_change(self):
+        key = self.page.locator(
+            ".nhsuk-summary-list__key", has=self.page.get_by_text("Lump", exact=True)
+        )
+        row = self.page.locator(".nhsuk-summary-list__row").filter(has=key)
+        row.get_by_text("Change").click()
+
+    def then_less_than_three_months_should_be_selected(self):
+        expect(self.page.get_by_label("Less than 3 months")).to_be_checked()
+
+    def and_i_see_three_months_to_a_year(self):
+        key = self.page.locator(
+            ".nhsuk-summary-list__key", has=self.page.get_by_text("Lump", exact=True)
+        )
+        row = self.page.locator(".nhsuk-summary-list__row").filter(has=key)
+        expect(row).to_contain_text("3 months to a year")
