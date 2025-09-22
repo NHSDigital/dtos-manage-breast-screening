4fb526e16bb891ae4a9931d2ef9a5140bbedabd9
Mat Moore <MatMoore@users.noreply.github.com>
2025-09-22 11:47:40 +0100

    add helper to convert boolean to Yes/No options

--- manage_breast_screening/nhsuk_forms/utils.py
+++ manage_breast_screening/nhsuk_forms/utils.py
@@ -4,3 +4,10 @@ from .fields.choice_fields import ChoiceField
 
 def yes_no_field(**kwargs) -> ChoiceField:
     return ChoiceField(choices=YesNo, **kwargs)
+
+
+def yes_no(bool):
+    if bool:
+        return YesNo.YES
+    else:
+        return YesNo.NO
