ef5b428f5ce16a4ef7e403db72f5b5615273d50a
Steve Laing <steve.laing@gmail.com>
2025-10-02 15:40:24 +0100

    Remove leading slash from callback path
    This is added in core urls for the notifications namespace.

--- manage_breast_screening/notifications/urls.py
+++ manage_breast_screening/notifications/urls.py
@@ -6,7 +6,7 @@ app_name = "notifications"
 
 urlpatterns = [
     path(
-        "/message-status/create",
+        "message-status/create",
         views.create_message_status,
         name="create_message_status",
     ),
