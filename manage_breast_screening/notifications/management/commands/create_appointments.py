d4c9dc152086120730c3fd69d6aeb2dacaca3ee2
Nimmo <dnimmo@gmail.com>
2025-10-02 15:05:17 +0100

    add aliased column function - PR comment

--- manage_breast_screening/notifications/management/commands/create_appointments.py
+++ manage_breast_screening/notifications/management/commands/create_appointments.py
@@ -113,19 +113,28 @@ class Command(BaseCommand):
                     nbss_id=row["Appointment ID"],
                     nhs_number=row["NHS Num"],
                     number=row["Screen Appt num"],
-                    batch_id=row.get("Batch ID", row.get("BatchID")),
+                    batch_id=row.get(
+                        self.handle_aliased_column("Batch ID", "BatchID", row)
+                    ),
                     clinic=clinic,
                     episode_started_at=datetime.strptime(
                         row["Episode Start"], "%Y%m%d"
                     ).replace(tzinfo=TZ_INFO),
-                    episode_type=row.get("Episode Type", row.get("Epsiode Type")),
+                    episode_type=row.get(
+                        self.handle_aliased_column("Episode Type", "Epsiode Type", row)
+                    ),
                     starts_at=self.appointment_date_and_time(row),
                     status=row["Status"],
                     booked_by=row["Booked By"],
                     booked_at=self.workflow_action_date_and_time(
                         row["Action Timestamp"]
                     ),
-                    assessment=self.is_assessment(row),
+                    assessment=row.get(
+                        self.handle_aliased_column(
+                            "Screen or Assess", "Screen or Asses", row
+                        )
+                    )
+                    == "A",
                 ),
                 True,
             )
@@ -163,15 +172,12 @@ class Command(BaseCommand):
         dt = datetime.strptime(timestamp, "%Y%m%d-%H%M%S")
         return dt.replace(tzinfo=TZ_INFO)
 
-    def is_assessment(self, row: pandas.Series) -> bool:
-        """
-        There was an error in the data where the column was initially misspelled as 'Screen or Asses'. To ensure backwards compatibility, we check for both spellings.
-        """
-        if "Screen or Assess" in row:
-            return row["Screen or Assess"] == "A"
-        elif "Screen or Asses" in row:
-            return row["Screen or Asses"] == "A"
-        return False
+    def handle_aliased_column(
+        self, expected_name: str, fallback_name: str, row: pandas.Series
+    ) -> str:
+        if expected_name in row:
+            return row[expected_name]
+        return row[fallback_name]
 
     def appointment_date_and_time(self, row: pandas.Series) -> datetime:
         dt = datetime.strptime(
