1287c66577d68a53d2b72b0bcf9f5dd662bf764f
Alastair Lock <alastair.lock@gmail.com>
2025-10-23 09:21:21 +0100

    add queue length alert

--- infrastructure/environments/review/variables.sh
+++ infrastructure/environments/review/variables.sh
@@ -2,6 +2,6 @@ ENV_CONFIG=review
 AZURE_SUBSCRIPTION="Digital Screening DToS - Core Services Dev"
 HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
 HUB=dev
-TERRAFORM_MODULES_REF=main
+TERRAFORM_MODULES_REF=feat/DTOSS-11379-Azure-monitor-infrastructure-metrics-on-storage-queues
 ENABLE_SOFT_DELETE=false
 ADO_MANAGEMENT_POOL=private-pool-dev-uks
