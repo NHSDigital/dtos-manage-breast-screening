dc85ed1b76a69cabc1559509acca1746cbc18032
Steve Laing <steve.laing@gmail.com>
2025-10-23 16:53:49 +0100

    Add an integration test to verify blob file ordering
    The Azure storage blob SDK for python provides a list_blobs method on the container client. We may receive multiple files per day and it is important they are processed in the correct sequential order denoted by filename.
    This commit adds a test to show that for an appointment booked in one file and cancelled in another, the file with the booking is processed before the file with the cancellation.

--- manage_breast_screening/notifications/tests/integration/test_create_appointments_from_azure_storage.py
+++ manage_breast_screening/notifications/tests/integration/test_create_appointments_from_azure_storage.py
@@ -27,10 +27,10 @@ class TestCreateAppointmentsFromAzureStorage:
     @pytest.mark.django_db
     def test_appointments_created_from_file_stored_in_azure(self, helpers):
         today_dirname = datetime.today().strftime("%Y-%m-%d")
-        test_file_path = "ABC_20241202091221_APPT_106.dat"
-        blob_name = f"{today_dirname}/{test_file_path}"
+        test_file_name = "ABC_20241202091221_APPT_106.dat"
+        blob_name = f"{today_dirname}/{test_file_name}"
 
-        with open(helpers.get_test_file_path(test_file_path)) as test_file:
+        with open(helpers.get_test_file_path(test_file_name)) as test_file:
             BlobStorage().add(blob_name, test_file.read())
 
         Command().handle(**{"date_str": today_dirname})
@@ -75,3 +75,27 @@ class TestCreateAppointmentsFromAzureStorage:
 
         assert appointments[0].clinic == clinics[1]
         assert appointments[1].clinic == clinics[1]
+
+    @pytest.mark.django_db
+    def test_files_are_processed_in_correct_order(self, helpers):
+        today_dirname = datetime.today().strftime("%Y-%m-%d")
+        test_file_names = [
+            "ABC_20241202091221_APPT_106.dat",
+            "ABC_20241202091321_APPT_107.dat",
+        ]
+        for test_file_name in test_file_names:
+            blob_name = f"{today_dirname}/{test_file_name}"
+
+            with open(helpers.get_test_file_path(test_file_name)) as test_file:
+                BlobStorage().add(blob_name, test_file.read())
+
+        Command().handle(**{"date_str": today_dirname})
+
+        assessment = Appointment.objects.filter(nhs_number="9449306621").first()
+        booked_then_cancelled = Appointment.objects.filter(
+            nhs_number="9449305552"
+        ).first()
+        assert booked_then_cancelled.updated_at is not None
+        assert booked_then_cancelled.cancelled_at is not None
+        assert assessment.created_at < booked_then_cancelled.updated_at
+        assert booked_then_cancelled.booked_at < booked_then_cancelled.cancelled_at
