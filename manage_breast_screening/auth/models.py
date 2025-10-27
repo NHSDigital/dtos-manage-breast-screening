29d043e3027cd17a04fa0b3c5f1476e935ba777a
Mat <MatMoore@users.noreply.github.com>
2025-10-27 09:17:59 +0000

    Update manage_breast_screening/auth/models.py
    Co-authored-by: Malcolm Baig <git@baig.org>

--- manage_breast_screening/auth/models.py
+++ manage_breast_screening/auth/models.py
@@ -9,7 +9,7 @@ class Role(StrEnum):
 
 class Permission(StrEnum):
     VIEW_PARTICIPANT_DATA = "participants.view_participant_data"
-    VIEW_MAMMOGRAM_APPOINTMENT = "mammograms.perform_mammogram_appointment"
+    VIEW_MAMMOGRAM_APPOINTMENT = "mammograms.view_mammogram_appointment"
     START_MAMMOGRAM_APPOINTMENT = "mammograms.start_mammogram_appointment"
 
 
