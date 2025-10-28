98e02dd1774399198b831c8ab82656756670d4a3
Mat Moore <MatMoore@users.noreply.github.com>
2025-10-28 11:08:25 +0000

    adjust html tests in accordance with design system updates

--- manage_breast_screening/nhsuk_forms/tests/jinja2/test_django_form_helpers.py
+++ manage_breast_screening/nhsuk_forms/tests/jinja2/test_django_form_helpers.py
@@ -1,6 +1,7 @@
 from textwrap import dedent
 
 from django.forms import ChoiceField, Form
+from pytest_django.asserts import assertHTMLEqual
 
 
 class ABForm(Form):
@@ -38,19 +39,20 @@ class TestDjangoFormHelpers:
 
         response = template.render({"form": form})
 
-        assert (
-            dedent(response).strip()
-            == dedent(
-                """
-                <div class="nhsuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabindex="-1" data-module="nhsuk-error-summary">
-                  <h2 class="nhsuk-error-summary__title" id="error-summary-title">
-                    There is a problem
-                  </h2>
-                  <div class="nhsuk-error-summary__body">
-                    <ul class="nhsuk-list nhsuk-error-summary__list" role="list">      <li>        <a href="#id_ab_choice">This field is required.</a>
-                </li>    </ul>
-                  </div>
+        assertHTMLEqual(
+            response,
+            """
+                <div class="nhsuk-error-summary" data-module="nhsuk-error-summary">
+                    <div role="alert">
+                        <h2 class="nhsuk-error-summary__title">
+                            There is a problem
+                        </h2>
+                        <div class="nhsuk-error-summary__body">
+                            <ul class="nhsuk-list nhsuk-error-summary__list">
+                                <li><a href="#id_ab_choice">This field is required.</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                """
-            ).strip()
+            """,
         )
