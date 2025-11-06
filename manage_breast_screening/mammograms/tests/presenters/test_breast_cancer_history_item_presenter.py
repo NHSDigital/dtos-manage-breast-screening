9ebd766b0e2cb2904cbd7af5e613a4fc5c3e3022
Mat Moore <MatMoore@users.noreply.github.com>
2025-11-06 11:49:49 +0000

    add a basic presenter for breast cancer history items
    This is just a placeholder for now; we want to reflect the data
    on the record medical information page, so we can start implementing
    the forms.
    
    Later we will need to deal with formatting information for the
    right/left breast side to side, and allow for hiding optional
    information that is not provided.

---
+++ manage_breast_screening/mammograms/tests/presenters/test_breast_cancer_history_item_presenter.py
@@ -0,0 +1,75 @@
+from manage_breast_screening.mammograms.presenters.breast_cancer_history_item_presenter import (
+    BreastCancerHistoryItemPresenter,
+)
+from manage_breast_screening.participants.models.breast_cancer_history_item import (
+    BreastCancerHistoryItem,
+)
+from manage_breast_screening.participants.tests.factories import (
+    BreastCancerHistoryItemFactory,
+)
+
+
+class TestBreastCancerHistoryItemPresenter:
+    def test_single(self):
+        item = BreastCancerHistoryItemFactory.build(
+            diagnosis_location=BreastCancerHistoryItem.DiagnosisLocationChoices.RIGHT_BREAST,
+            right_breast_procedure=BreastCancerHistoryItem.Procedure.LUMPECTOMY,
+            intervention_location=BreastCancerHistoryItem.InterventionLocation.NHS_HOSPITAL,
+            intervention_location_details="East Tester Hospital",
+            additional_details="some details",
+        )
+
+        presenter = BreastCancerHistoryItemPresenter(item)
+
+        assert presenter.summary_list_params == {
+            "rows": [
+                {
+                    "key": {
+                        "text": "Cancer location",
+                    },
+                    "value": {
+                        "html": "Right breast",
+                    },
+                },
+                {
+                    "key": {
+                        "text": "Procedures",
+                    },
+                    "value": {
+                        "html": "Right breast: Lumpectomy<br>Left breast: No procedure",
+                    },
+                },
+                {
+                    "key": {
+                        "text": "Other surgery",
+                    },
+                    "value": {
+                        "html": "Right breast: No surgery<br>Left breast: No surgery",
+                    },
+                },
+                {
+                    "key": {
+                        "text": "Treatment",
+                    },
+                    "value": {
+                        "html": "Right breast: No radiotherapy<br>Left breast: No radiotherapy<br>Systemic treatements: ",
+                    },
+                },
+                {
+                    "key": {
+                        "text": "Treatment location",
+                    },
+                    "value": {
+                        "html": "At an NHS hospital: East Tester Hospital",
+                    },
+                },
+                {
+                    "key": {
+                        "text": "Additional details",
+                    },
+                    "value": {
+                        "html": "some details",
+                    },
+                },
+            ],
+        }
