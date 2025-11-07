9a7d6494ef30665974d7d204f24ca4ca684e30c4
Steve Webber <198724792+swebberuk@users.noreply.github.com>
2025-11-07 14:24:51 +0000

    Added implanted_medical_device_history_item table.
    Added demo data and added Implanted medical device history details to
    the Medical history section of the Record medical information page.

--- manage_breast_screening/participants/models/__init__.py
+++ manage_breast_screening/participants/models/__init__.py
@@ -1,6 +1,7 @@
 from .appointment import Appointment, AppointmentStatus
 from .breast_cancer_history_item import BreastCancerHistoryItem
 from .ethnicity import Ethnicity
+from .implanted_medical_device_history_item import ImplantedMedicalDeviceHistoryItem
 from .participant import Participant, ParticipantAddress
 from .reported_mammograms import ParticipantReportedMammogram, SupportReasons
 from .screening_episode import ScreeningEpisode
@@ -10,6 +11,7 @@ __all__ = [
     "Appointment",
     "AppointmentStatus",
     "BreastCancerHistoryItem",
+    "ImplantedMedicalDeviceHistoryItem",
     "Participant",
     "ParticipantAddress",
     "Ethnicity",
---
+++ manage_breast_screening/participants/models/implanted_medical_device_history_item.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from ...core.models import BaseModel
+from .appointment import Appointment
+
+
+class ImplantedMedicalDeviceHistoryItem(BaseModel):
+    class Device(models.TextChoices):
+        CARDIAC_DEVICE = "CARDIAC_DEVICE", "Cardiac device (such as a pacemaker or ICD)"
+        HICKMAN_LINE = "HICKMAN_LINE", "Hickman line"
+        OTHER_MEDICAL_DEVICE = "OTHER_MEDICAL_DEVICE", "Other medical device"
+
+    appointment = models.ForeignKey(
+        Appointment,
+        on_delete=models.PROTECT,
+        related_name="implanted_medical_device_history_items",
+    )
+    device = models.CharField(choices=Device)
+    other_medical_device_details = models.CharField(blank=True, null=False, default="")
+    procedure_year = models.IntegerField(null=True)
+    removal_year = models.IntegerField(null=True)
+    additional_details = models.TextField(blank=True, null=False, default="")
