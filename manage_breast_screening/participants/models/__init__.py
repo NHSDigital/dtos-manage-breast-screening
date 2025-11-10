fb8d8193aa4f8f53339d2fa72672125fa399040c
Malcolm Baig <git@baig.org>
2025-11-10 15:31:13 +0000

    Add BenignLumpHistoryItem model and migration

--- manage_breast_screening/participants/models/__init__.py
+++ manage_breast_screening/participants/models/__init__.py
@@ -1,4 +1,5 @@
 from .appointment import Appointment, AppointmentStatus
+from .benign_lump_history_item import BenignLumpHistoryItem
 from .breast_augmentation_history_item import BreastAugmentationHistoryItem
 from .breast_cancer_history_item import BreastCancerHistoryItem
 from .ethnicity import Ethnicity
@@ -12,6 +13,7 @@ from .symptom import Symptom, SymptomAreas, SymptomSubType, SymptomType
 __all__ = [
     "Appointment",
     "AppointmentStatus",
+    "BenignLumpHistoryItem",
     "BreastAugmentationHistoryItem",
     "BreastCancerHistoryItem",
     "ImplantedMedicalDeviceHistoryItem",
