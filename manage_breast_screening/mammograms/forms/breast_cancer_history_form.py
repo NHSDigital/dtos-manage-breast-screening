935238541a6edee38188f5e2d43bab7c2585bf4a
Mat Moore <MatMoore@users.noreply.github.com>
2025-11-12 12:41:41 +0000

    start form for breast cancer history
    There are two new patterns here for arranging checkboxes
    and radios into a grid. This is because we want to show
    options relating to the right breast on the left
    and left breast on the right, to match the perspective
    of the mammographer facing the participant.
    
    In one case, we are treating right breast and left breast as separate
    fields, and rendering them side by side. Both fields have their own
    legends and are validated independently.
    
    In the other case, we model the question as a single field and we just
    arrange the options. We implement this by calling the component twice
    for the same field, but passing in different options.

--- manage_breast_screening/mammograms/forms/breast_cancer_history_form.py
+++ manage_breast_screening/mammograms/forms/breast_cancer_history_form.py
@@ -1,5 +1,94 @@
+from django.db.models import TextChoices
+from django.forms import Textarea
+
+from manage_breast_screening.nhsuk_forms.fields.char_field import CharField
+from manage_breast_screening.nhsuk_forms.fields.choice_fields import (
+    ChoiceField,
+    MultipleChoiceField,
+)
+from manage_breast_screening.nhsuk_forms.fields.integer_field import IntegerField
 from manage_breast_screening.nhsuk_forms.forms import FormWithConditionalFields
+from manage_breast_screening.participants.models.breast_cancer_history_item import (
+    BreastCancerHistoryItem,
+)
 
 
 class BreastCancerHistoryForm(FormWithConditionalFields):
-    pass
+    class DiagnosisLocationChoices(TextChoices):
+        RIGHT_BREAST = "RIGHT_BREAST", "Right breast"
+        LEFT_BREAST = "LEFT_BREAST", "Left breast"
+        DONT_KNOW = "DONT_KNOW", "Don't know"
+
+    diagnosis_location = MultipleChoiceField(
+        label="In which breasts was cancer diagnosed?", choices=DiagnosisLocationChoices
+    )
+    # todo: constrain min/max
+    diagnosis_year = IntegerField(
+        label="Year of diagnosis (optional)",
+        label_classes="nhsuk-label--m",
+        classes="nhsuk-input--width-4",
+        hint="Leave blank if unknown",
+    )
+
+    left_breast_procedure = ChoiceField(
+        label="Left breast (or axilla)",
+        visually_hidden_label_prefix="What procedure have they had in their ",
+        visually_hidden_label_suffix="?",
+        label_classes="nhsuk-fieldset__legend--s",
+        choices=BreastCancerHistoryItem.Procedure,
+    )
+    right_breast_procedure = ChoiceField(
+        label="Right breast (or axilla)",
+        visually_hidden_label_prefix="What procedure have they had in their ",
+        visually_hidden_label_suffix="?",
+        label_classes="nhsuk-fieldset__legend--s",
+        choices=BreastCancerHistoryItem.Procedure,
+    )
+
+    left_breast_other_surgery = MultipleChoiceField(
+        label="Left breast (or axilla)",
+        visually_hidden_label_prefix="What other surgery have they had in their ",
+        visually_hidden_label_suffix="?",
+        label_classes="nhsuk-fieldset__legend--s",
+        choices=BreastCancerHistoryItem.Surgery,
+    )
+    right_breast_other_surgery = MultipleChoiceField(
+        label="Right breast (or axilla)",
+        visually_hidden_label_prefix="What other surgery have they had in their ",
+        visually_hidden_label_suffix="?",
+        label_classes="nhsuk-fieldset__legend--s",
+        choices=BreastCancerHistoryItem.Surgery,
+    )
+
+    left_breast_treatment = MultipleChoiceField(
+        label="Left breast (or axilla)",
+        visually_hidden_label_prefix="What treatment have they had in their ",
+        visually_hidden_label_suffix="?",
+        choices=BreastCancerHistoryItem.Treatment,
+    )
+    right_breast_treatment = MultipleChoiceField(
+        label="Right breast (or axilla)",
+        visually_hidden_label_prefix="What treatment have they had in their ",
+        visually_hidden_label_suffix="?",
+        choices=BreastCancerHistoryItem.Treatment,
+    )
+
+    systemic_treatments = ChoiceField(
+        visually_hidden_label_prefix="What treatment have they had that are ",
+        visually_hidden_label_suffix="?",
+        label="Systemic treatments",
+        choices=BreastCancerHistoryItem.SystemicTreatment,
+    )
+    systemic_treatment_other_treatment_details = CharField(
+        label="Provide details", required=False
+    )
+
+    intervention_location = ChoiceField(
+        label="Where did surgery and treatment take place?",
+        choices=BreastCancerHistoryItem.InterventionLocation,
+    )
+    intervention_location_details = CharField(label="Provide details", required=False)
+
+    additional_details = CharField(
+        label="Additional details (optional)", required=False, widget=Textarea
+    )
