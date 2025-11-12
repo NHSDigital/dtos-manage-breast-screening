5c7297cdc0b5f1fd6fc9ddbc658f50adbe56349e
Nimmo <dnimmo@gmail.com>
2025-11-12 11:30:18 +0000

    re-add comment

--- manage_breast_screening/notifications/presenters/clinic_location_data.py
+++ manage_breast_screening/notifications/presenters/clinic_location_data.py
@@ -118,6 +118,7 @@ class ClinicLocationData:
         self.code = clinic.code
         self.bso_code = clinic.bso_code
         self.data = self.location_data()
+        # Not used, but still part of the Notify template, so fails validation if not present.
         self.description = ""
         self.url = self.data.get("location_url", "")
 
