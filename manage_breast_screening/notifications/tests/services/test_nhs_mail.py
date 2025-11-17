328e0a0585c28db92f304304647468456025203f
Steve Laing <steve.laing@gmail.com>
2025-11-17 11:52:43 +0000

    Amend service to send all reports in one email

--- manage_breast_screening/notifications/tests/services/test_nhs_mail.py
+++ manage_breast_screening/notifications/tests/services/test_nhs_mail.py
@@ -38,16 +38,18 @@ class TestNhsMail:
             "NOTIFICATIONS_SMTP_RECIPIENTS", "recipient@nhsmail.net,another@nhs.net"
         )
         subject = NhsMail()
-        subject.send_report_email(csv_data, "filename.csv", "invites_not_sent")
+        subject.send_reports_email({"filename.csv": csv_data})
 
         mock_smtp_server.login.assert_called_once_with("sender@nhsmail.net", "password")
         mock_smtp_server.sendmail.assert_called_once_with(
             "sender@nhsmail.net", ["recipient@nhsmail.net", "another@nhs.net"], ANY
         )
 
-    def test_sends_a_failure_report_email(self, mock_smtp_server, csv_data):
+    def test_sends_a_reports_email(self, mock_smtp_server, csv_data):
         subject = NhsMail()
-        subject.send_report_email(csv_data, "filename.csv", "invites_not_sent")
+        subject.send_reports_email(
+            {"filename1.csv": csv_data, "filename2.csv": csv_data}
+        )
 
         mock_smtp_server.login.assert_called_once_with("sender@nhsmail.net", "password")
         mock_smtp_server.sendmail.assert_called_once_with(
@@ -59,62 +61,29 @@ class TestNhsMail:
 
         decoded_subject_line = str(make_header(decode_header(mime_message["Subject"])))
         assert (
-            "[TEST] Breast screening digital comms invites not sent report – 11-10-2025 – Birmingham (MCR)"
+            "[TEST] Breast screening digital comms reports – 11-10-2025 – Birmingham (MCR)"
             in decoded_subject_line
         )
         assert "Content-Type: text/html" in email_content
-        assert (
-            "It's important that you take action when you receive this email"
-            in email_content
-        )
 
-        assert "filename.csv" in email_content
-        decoded_attachment_data = (
-            mime_message.get_payload()[1]  # type: ignore
-            .get_payload(  # type: ignore
-                None, True
+        assert "filename1.csv" in email_content
+        assert "filename2.csv" in email_content
+        for index in [1, 2]:
+            decoded_attachment_data = (
+                mime_message.get_payload()[index]  # type: ignore
+                .get_payload(  # type: ignore
+                    None, True
+                )
+                .decode("utf-8")  # type: ignore
             )
-            .decode("utf-8")  # type: ignore
-        )
-        assert csv_data in decoded_attachment_data
-
-    def test_sends_an_aggregate_report_email(self, mock_smtp_server, csv_data):
-        subject = NhsMail()
-        subject.send_report_email(csv_data, "filename.csv", "aggregate")
-
-        mock_smtp_server.login.assert_called_once_with("sender@nhsmail.net", "password")
-        mock_smtp_server.sendmail.assert_called_once_with(
-            "sender@nhsmail.net", ["recipient@nhsmail.net"], ANY
-        )
-        email_content = mock_smtp_server.sendmail.call_args[0][2]
-        mime_message = email.message_from_string(email_content)
-
-        decoded_subject_line = str(make_header(decode_header(mime_message["Subject"])))
-
-        assert (
-            "[TEST] Breast screening digital comms aggregate report – 11-10-2025 – Birmingham (MCR)"
-            in decoded_subject_line
-        )
-        assert (
-            "Attached to the email is the daily report for breast screening digital"
-            in email_content
-        )
-        assert "filename.csv" in email_content
-        decoded_attachment_data = (
-            mime_message.get_payload()[1]  # type: ignore
-            .get_payload(  # type: ignore
-                None, True
-            )
-            .decode("utf-8")  # type: ignore
-        )
-        assert csv_data in decoded_attachment_data
+            assert csv_data in decoded_attachment_data
 
     def test_raises_exception_if_email_fails(self, mock_smtp_server, csv_data):
         mock_smtp_server.sendmail.side_effect = smtplib.SMTPException
 
         with pytest.raises(smtplib.SMTPException):
             subject = NhsMail()
-            subject.send_report_email(csv_data, "filename.csv")
+            subject.send_reports_email({"filename.csv": csv_data})
 
     def test_does_not_send_email_if_disabled(
         self, mock_smtp_server, csv_data, monkeypatch
@@ -122,7 +91,7 @@ class TestNhsMail:
         monkeypatch.setenv("NOTIFICATIONS_SMTP_IS_ENABLED", "False")
 
         subject = NhsMail()
-        subject.send_report_email(csv_data, "filename.csv", "invites_not_sent")
+        subject.send_reports_email({"filename.csv": csv_data})
 
         mock_smtp_server.ehlo.assert_not_called()
         mock_smtp_server.starttls.assert_not_called()
@@ -135,13 +104,13 @@ class TestNhsMail:
         monkeypatch.setenv("DJANGO_ENV", "prod")
 
         subject = NhsMail()
-        subject.send_report_email(csv_data, "filename.csv", "invites_not_sent")
+        subject.send_reports_email({"filename.csv": csv_data})
 
         email_content = mock_smtp_server.sendmail.call_args[0][2]
         mime_message = email.message_from_string(email_content)
 
         decoded_subject_line = str(make_header(decode_header(mime_message["Subject"])))
         assert (
-            "Breast screening digital comms invites not sent report – 11-10-2025 – Birmingham (MCR)"
+            "Breast screening digital comms reports – 11-10-2025 – Birmingham (MCR)"
             in decoded_subject_line
         )
