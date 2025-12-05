63f82ca750f5cfa1b4f3cdeb441b3408896a90b8
Alastair Lock <alastair.lock@gmail.com>
2025-12-05 13:58:20 +0000

    wip

--- infrastructure/environments/review/variables.sh
+++ infrastructure/environments/review/variables.sh
@@ -4,6 +4,5 @@ HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
 HUB=dev
 TERRAFORM_MODULES_REF=DTOSS-11646-Setup-Container-Apps-container-probes
 ENABLE_SOFT_DELETE=false
-
 ADO_MANAGEMENT_POOL=private-pool-dev-uks
 RUN_NOTIFICATIONS_SMOKE_TEST=false
