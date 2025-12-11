b0a5909d09d4b81959f07cc0035547dc9f085148
Malcolm Baig <git@baig.org>
2025-12-11 12:56:45 +0000

    Update appointment note system spec to cover deletion

--- manage_breast_screening/tests/system/clinical/test_appointment_note.py
+++ manage_breast_screening/tests/system/clinical/test_appointment_note.py
@@ -21,14 +21,23 @@ class TestAppointmentNote(SystemTestCase):
         self.when_i_enter_a_note()
         self.and_i_save_the_note()
         self.then_i_see_a_message_confirming_the_save()
-        self.then_the_note_field_contains(self.initial_note_text)
+        self.and_the_note_field_contains(self.initial_note_text)
         self.and_the_appointment_details_tab_shows_the_note(self.initial_note_text)
 
         self.when_i_update_the_note()
         self.and_i_save_the_note()
         self.then_i_see_a_message_confirming_the_save()
-        self.then_the_note_field_contains(self.updated_note_text)
+        self.and_the_note_field_contains(self.updated_note_text)
         self.and_the_appointment_details_tab_shows_the_note(self.updated_note_text)
+
+        self.when_i_click_the_note_change_link()
+        self.and_i_click_delete_note()
+        self.and_i_click_cancel()
+        self.then_the_note_is_not_deleted()
+
+        self.when_i_click_delete_note()
+        self.and_i_confirm_deletion()
+        self.then_the_note_is_deleted()
 
     def and_there_is_an_appointment_for_my_provider(self):
         self.appointment = AppointmentFactory(
@@ -67,7 +76,7 @@ class TestAppointmentNote(SystemTestCase):
     def when_i_save_the_note(self):
         self.and_i_save_the_note()
 
-    def then_the_note_field_contains(self, text):
+    def and_the_note_field_contains(self, text):
         expect(self.page.get_by_label("Note")).to_have_value(text)
 
     def then_i_see_a_message_confirming_the_save(self):
@@ -86,3 +95,38 @@ class TestAppointmentNote(SystemTestCase):
         )
         expect(note_container).to_be_visible()
         expect(note_container).to_contain_text(text)
+
+    def when_i_click_the_note_change_link(self):
+        note_container = self.page.locator(
+            ".nhsuk-warning-callout", has_text="Appointment note"
+        )
+        expect(note_container).to_be_visible()
+        change_link = note_container.get_by_role("link", name="Change")
+        change_link.click()
+
+    def and_i_click_delete_note(self):
+        delete_link = self.page.get_by_role("link", name="Delete appointment note")
+        expect(delete_link).to_be_visible()
+        delete_link.click()
+
+    def when_i_click_delete_note(self):
+        self.and_i_click_delete_note()
+
+    def and_i_click_cancel(self):
+        cancel_link = self.page.get_by_role("link", name="Cancel")
+        expect(cancel_link).to_be_visible()
+        cancel_link.click()
+
+    def then_the_note_is_not_deleted(self):
+        expect(self.page.get_by_label("Note")).to_have_value(self.updated_note_text)
+
+    def and_i_confirm_deletion(self):
+        delete_button = self.page.get_by_role("button", name="Delete appointment note")
+        expect(delete_button).to_be_visible()
+        delete_button.click()
+
+    def then_the_note_is_deleted(self):
+        banner = self.page.locator(".nhsuk-notification-banner--success")
+        expect(banner).to_be_visible()
+        expect(banner).to_contain_text("Appointment note deleted")
+        expect(self.page.get_by_label("Note")).to_have_value("")
