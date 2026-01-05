52aa18300a96cb4cec1b59bab1289a0135640f0c
Mat Moore <MatMoore@users.noreply.github.com>
2026-01-05 10:06:44 +0000

    configure axe using run tags
    This enables some checks for AAA level and best practice.
    Configuration taken from nhsuk-frontend.

--- manage_breast_screening/core/utils/acessibility.py
+++ manage_breast_screening/core/utils/acessibility.py
@@ -4,14 +4,38 @@ from axe_playwright_python.sync_playwright import Axe
 AXE_VIOLATIONS_EXCLUDE_LIST = [
     "region",  # 'Some page content is not contained by landmarks' https://github.com/alphagov/govuk-frontend/issues/1604
     "aria-allowed-attr",  # 'ARIA attribute is not allowed: aria-expanded="false"' https://github.com/alphagov/govuk-frontend/issues/979
+    "color-contrast-enhanced",  # Ignore 'Element has insufficient color contrast' for WCAG Level AAA
 ]
 
 
 class AxeAdapter(Axe):
     def __init__(self):
+        # fmt: off
         self.default_options = {
-            "rules": {id: {"enabled": False} for id in AXE_VIOLATIONS_EXCLUDE_LIST}
+            "rules": {
+                id: {"enabled": False} for id in AXE_VIOLATIONS_EXCLUDE_LIST
+            },
+            "runOnly": {
+                "type": "tag",
+                "values": [
+                    "best-practice",
+
+                    # WCAG 2.x
+                    "wcag2a",
+                    "wcag2aa",
+                    "wcag2aaa",
+
+                    # WCAG 2.1
+                    "wcag21a",
+                    "wcag21aa",
+
+                    # WCAG 2.2
+                    "wcag22aa"
+                ]
+            }
         }
+        # fmt: on
+
         super().__init__()
 
     def run(self, page, context=None, options=None) -> AxeResults:
