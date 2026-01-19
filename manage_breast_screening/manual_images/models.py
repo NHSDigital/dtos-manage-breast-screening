199bebdf76ab7ebe434a911fac507750b9d099e0
Graham Pengelly <graham@three-tier.com>
2026-01-19 11:46:22 +0000

    Manual image flow models and app
    * Add manual_images app
    * Add Study and Series models
    
    We are building functionality to allow some data to be recorded about
    image capture without the gateway being ready/available.
    
    We've decided to separate this out into its own app as it is distinct
    from the automatic image flow and it will be easier to manage, and to
    possibly remove if we keep it as isolated as possible.

---
+++ manage_breast_screening/manual_images/models.py
@@ -0,0 +1,35 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from manage_breast_screening.core.models import BaseModel
+
+
+class Study(BaseModel):
+    appointment = models.ForeignKey(
+        "participants.Appointment", on_delete=models.PROTECT
+    )
+
+    additional_details = models.TextField(blank=True, null=False, default="")
+
+
+class Series(BaseModel):
+    study = models.ForeignKey(Study, on_delete=models.PROTECT)
+
+    view_position = models.CharField(
+        max_length=10,
+        blank=True,
+        help_text="e.g., CC, MLO",
+        choices=[("CC", "CC"), ("MLO", "MLO"), ("EKLUND", "EKLUND")],
+    )
+    laterality = models.CharField(
+        max_length=1,
+        blank=True,
+        help_text="L or R",
+        choices=[("L", "Left"), ("R", "Right")],
+    )
+    count = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(20)]
+    )
+
+    class Meta:
+        unique_together = ["study", "view_position", "laterality"]
