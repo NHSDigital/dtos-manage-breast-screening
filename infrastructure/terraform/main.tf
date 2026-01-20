24a9955ce2f42c871cc2367ffd73008fc72919e5
Josiel Souza <josiel.souza@nordcloud.com>
2026-01-20 10:34:38 +0000

    feat(infra): add support for Azure Arc-enabled servers
       - Introduce 'enable_arc_servers' feature flag to conditionally deploy Arc resources.
       - Added resource groups `rg-manbrs-dev-uks-arc-enabled-servers` and `rg-manbrs-dev-uks-arc-automation`.
       - Update 'resource_group_init' Bicep to allow the Managed Identity assign the 'Azure Connected Machine Onboarding' role.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -20,6 +20,7 @@ module "infra" {
   cae_zone_redundancy_enabled = var.cae_zone_redundancy_enabled
   enable_alerting             = var.enable_alerting
   action_group_id             = var.deploy_infra ? module.infra[0].monitor_action_group_id : data.azurerm_monitor_action_group.main[0].id
+  enable_arc_servers          = var.enable_arc_servers
 }
 
 module "shared_config" {
