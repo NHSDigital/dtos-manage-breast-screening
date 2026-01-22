f3aa795c0b6d8aafdac10f2e85e2fd306520a5da
Steve Laing <steve.laing@gmail.com>
2026-01-22 13:30:51 +0000

    Add indexes and unique constraints on Study, Series and Image

--- manage_breast_screening/dicom/models.py
+++ manage_breast_screening/dicom/models.py
@@ -7,6 +7,13 @@ def dicom_storage():
 
 
 class Study(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=["study_instance_uid"]),
+            models.Index(fields=["patient_id"]),
+            models.Index(fields=["source_message_id"]),
+        ]
+
     study_instance_uid = models.CharField(max_length=128, unique=True)
     source_message_id = models.CharField(max_length=128)
     patient_id = models.CharField(max_length=10, blank=True)
@@ -18,6 +25,17 @@ class Study(models.Model):
 
 
 class Series(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=["series_instance_uid"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["series_number", "study"],
+                name="unique_series_number_per_study",
+            )
+        ]
+
     series_instance_uid = models.CharField(max_length=128, unique=True)
     study = models.ForeignKey(Study, on_delete=models.CASCADE, related_name="series")
     modality = models.CharField(max_length=16, blank=True)
@@ -28,6 +46,17 @@ class Series(models.Model):
 
 
 class Image(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=["sop_instance_uid"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["instance_number", "series"],
+                name="unique_instance_number_per_series",
+            )
+        ]
+
     sop_instance_uid = models.CharField(max_length=128, unique=True)
     series = models.ForeignKey(Series, on_delete=models.CASCADE, related_name="images")
     instance_number = models.IntegerField(null=True, blank=True)
