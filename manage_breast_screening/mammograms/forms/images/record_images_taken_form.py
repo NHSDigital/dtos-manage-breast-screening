0ae3e67b3c935389ca823e89d0b1131bfc74ff63
Mat Moore <MatMoore@users.noreply.github.com>
2026-01-27 11:31:09 +0000

    add a form for the first step in recording images

---
+++ manage_breast_screening/mammograms/forms/images/record_images_taken_form.py
@@ -0,0 +1,36 @@
+from django.db.models import TextChoices
+from django.forms import Form
+
+from manage_breast_screening.nhsuk_forms.fields.choice_fields import ChoiceField
+
+
+class RecordImagesTakenForm(Form):
+    class StandardImagesChoices(TextChoices):
+        YES_TWO_CC_AND_TWO_MLO = "YES_TWO_CC_AND_TWO_MLO", "Yes, 2 CC and 2 MLO"
+        NO_ADD_ADDITIONAL = (
+            "NO_ADD_ADDITIONAL",
+            "No, add other information",
+        )
+        NO_IMAGES_TAKEN = "NO_IMAGES_TAKEN", "No images taken"
+
+    standard_images = ChoiceField(
+        choices=StandardImagesChoices,
+        required=True,
+        choice_hints={
+            StandardImagesChoices.NO_ADD_ADDITIONAL: "Such as too few or additional images",
+            StandardImagesChoices.NO_IMAGES_TAKEN: "The appointment cannot proceed",
+        },
+        label="Have you taken a standard set of images?",
+        error_messages={
+            "required": "Select whether you have taken a standard set of images or not"
+        },
+    )
+
+    def save(self, study_service):
+        if (
+            self.cleaned_data["standard_images"]
+            == self.StandardImagesChoices.YES_TWO_CC_AND_TWO_MLO
+        ):
+            study_service.create_with_default_series()
+        else:
+            study_service.remove_existing_study_and_series()
