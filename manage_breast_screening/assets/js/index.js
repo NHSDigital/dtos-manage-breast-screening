6f1f116883ac74b24054ee3953f24408c4c37f80
Mat Moore <MatMoore@users.noreply.github.com>
2026-01-28 15:15:08 +0000

    add stepper component
    This repurposes Colin's proposed code for the design system.
    See https://github.com/nhsuk/nhsuk-frontend/pull/1719
    
    I've stripped out a few options that we don't need right now
    in order to focus on the use case we have.
    
    Should this be accepted into the design system, we'll back out these
    changes, but the way we call the component will be very similar.

--- manage_breast_screening/assets/js/index.js
+++ manage_breast_screening/assets/js/index.js
@@ -1,6 +1,7 @@
 import { createAll, initAll } from 'nhsuk-frontend'
 
 import { CheckIn } from './check-in.js'
+import { StepperInput } from './stepper-input.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   // Initialise NHS.UK frontend components
@@ -8,4 +9,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Initialise application components
   createAll(CheckIn)
+  createAll(StepperInput)
 })
