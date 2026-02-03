513fb355719bce5eeef3e0f4ddf3f7e791e9d95b
Mat Moore <MatMoore@users.noreply.github.com>
2026-02-03 16:56:39 +0000

    add extra fields to image details form
    At a study level, record:
    
    - whether the study is complete
    - reasons why the study is incomplete
    - a flag for "imperfect but best possible"
    
    At the appointment level, record whether the participant needs to be
    reinvited. This currently just sets a flag, requiring users to manually
    reinvite the participant. We do the same thing when the appointment is
    terminated early before taking the images.
    
    fixup forms

--- manage_breast_screening/mammograms/tests/forms/test_add_image_details_form.py
+++ manage_breast_screening/mammograms/tests/forms/test_add_image_details_form.py
@@ -6,8 +6,24 @@ from django.http import QueryDict
 from manage_breast_screening.mammograms.forms.images.add_image_details_form import (
     AddImageDetailsForm,
 )
+from manage_breast_screening.mammograms.services.appointment_services import (
+    RecallService,
+)
+from manage_breast_screening.manual_images.models import (
+    IncompleteImagesReason,
+    StudyCompleteness,
+)
 from manage_breast_screening.manual_images.services import StudyService
-from manage_breast_screening.participants.tests.factories import AppointmentFactory
+
+
+@pytest.fixture
+def study_service(in_progress_appointment):
+    return StudyService(appointment=in_progress_appointment, current_user=None)
+
+
+@pytest.fixture
+def recall_service(in_progress_appointment):
+    return RecallService(appointment=in_progress_appointment, current_user=None)
 
 
 @pytest.mark.django_db
@@ -37,6 +53,11 @@ class TestAddImageDetailsForm:
                         "lcc_count": 0,
                         "left_eklund_count": 0,
                         "additional_details": "Some additional details",
+                        "not_all_mammograms_taken": False,
+                        "reasons_incomplete": [],
+                        "reasons_incomplete_details": "",
+                        "imperfect_but_best_possible": False,
+                        "should_recall": None,
                     },
                     doseq=True,
                 )
@@ -48,8 +69,7 @@ class TestAddImageDetailsForm:
             "__all__": ["Enter at least one image count"],
         }
 
-    def test_counts_provided_for_all_image_types(self):
-        appointment = AppointmentFactory()
+    def test_counts_provided_for_all_image_types(self, study_service, recall_service):
         rmlo_count = 7
         rcc_count = 5
         right_eklund_count = 20
@@ -68,6 +88,11 @@ class TestAddImageDetailsForm:
                         "lcc_count": lcc_count,
                         "left_eklund_count": left_eklund_count,
                         "additional_details": additional_details,
+                        "not_all_mammograms_taken": False,
+                        "reasons_incomplete": [],
+                        "reasons_incomplete_details": "",
+                        "imperfect_but_best_possible": False,
+                        "should_recall": None,
                     },
                     doseq=True,
                 )
@@ -76,10 +101,13 @@ class TestAddImageDetailsForm:
 
         assert form.is_valid()
 
-        study = form.save(StudyService(appointment=appointment, current_user=None))
+        study = form.save(study_service=study_service, recall_service=recall_service)
 
-        assert study.appointment == appointment
+        assert study.appointment == study_service.appointment
         assert study.additional_details == additional_details
+        assert study.completeness == StudyCompleteness.COMPLETE
+        assert not study.imperfect_but_best_possible
+        assert study.reasons_incomplete == []
 
         series_list = study.series_set.all()
         assert len(series_list) == 6
@@ -90,8 +118,10 @@ class TestAddImageDetailsForm:
         self._assert_series(series_list[4], "CC", "L", lcc_count)
         self._assert_series(series_list[5], "EKLUND", "L", left_eklund_count)
 
-    def test_counts_provided_for_only_one_image_type(self):
-        appointment = AppointmentFactory()
+    def test_counts_provided_for_only_one_image_type_and_should_recall(
+        self, study_service, recall_service
+    ):
+        appointment = study_service.appointment
         lmlo_count = 1
         form = AddImageDetailsForm(
             QueryDict(
@@ -103,6 +133,14 @@ class TestAddImageDetailsForm:
                         "lmlo_count": lmlo_count,
                         "lcc_count": 0,
                         "left_eklund_count": 0,
+                        "not_all_mammograms_taken": True,
+                        "reasons_incomplete": [
+                            IncompleteImagesReason.LANGUAGE_OR_LEARNING_DIFFICULTIES,
+                            IncompleteImagesReason.UNABLE_TO_SCAN_TISSUE,
+                        ],
+                        "reasons_incomplete_details": "",
+                        "imperfect_but_best_possible": False,
+                        "should_recall": AddImageDetailsForm.RecallChoices.TO_BE_RECALLED,
                     },
                     doseq=True,
                 )
@@ -111,14 +149,125 @@ class TestAddImageDetailsForm:
 
         assert form.is_valid()
 
-        study = form.save(StudyService(appointment=appointment, current_user=None))
+        study = form.save(study_service=study_service, recall_service=recall_service)
 
         assert study.appointment == appointment
+        assert study.completeness == StudyCompleteness.INCOMPLETE
+        assert study.reasons_incomplete == [
+            IncompleteImagesReason.LANGUAGE_OR_LEARNING_DIFFICULTIES,
+            IncompleteImagesReason.UNABLE_TO_SCAN_TISSUE,
+        ]
+        assert study.reasons_incomplete_details == ""
+        assert not study.imperfect_but_best_possible
         assert not study.additional_details
+        assert appointment.reinvite
 
         series_list = study.series_set.all()
         assert len(series_list) == 1
         self._assert_series(series_list[0], "MLO", "L", lmlo_count)
+
+    def test_counts_provided_for_only_one_image_type_and_should_not_recall(
+        self, study_service, recall_service
+    ):
+        appointment = study_service.appointment
+        lmlo_count = 1
+        form = AddImageDetailsForm(
+            QueryDict(
+                urlencode(
+                    {
+                        "rmlo_count": 0,
+                        "rcc_count": 0,
+                        "right_eklund_count": 0,
+                        "lmlo_count": lmlo_count,
+                        "lcc_count": 0,
+                        "left_eklund_count": 0,
+                        "not_all_mammograms_taken": True,
+                        "reasons_incomplete": [
+                            IncompleteImagesReason.UNABLE_TO_SCAN_TISSUE
+                        ],
+                        "reasons_incomplete_details": "",
+                        "imperfect_but_best_possible": True,
+                        "should_recall": AddImageDetailsForm.RecallChoices.PARTIAL_MAMMOGRAPHY,
+                    },
+                    doseq=True,
+                )
+            ),
+        )
+
+        assert form.is_valid()
+
+        study = form.save(study_service=study_service, recall_service=recall_service)
+
+        assert study.appointment == appointment
+        assert study.completeness == StudyCompleteness.PARTIAL
+        assert study.reasons_incomplete == [
+            IncompleteImagesReason.UNABLE_TO_SCAN_TISSUE,
+        ]
+        assert study.reasons_incomplete_details == ""
+        assert study.imperfect_but_best_possible
+        assert not study.additional_details
+        assert not appointment.reinvite
+
+        series_list = study.series_set.all()
+        assert len(series_list) == 1
+        self._assert_series(series_list[0], "MLO", "L", lmlo_count)
+
+    def test_not_all_mammograms_taken_but_not_marked_as_such(self):
+        form = AddImageDetailsForm(
+            QueryDict(
+                urlencode(
+                    {
+                        "rmlo_count": 1,
+                        "rcc_count": 1,
+                        "right_eklund_count": 0,
+                        "lmlo_count": 0,
+                        "lcc_count": 1,
+                        "left_eklund_count": 0,
+                        "not_all_mammograms_taken": False,
+                        "reasons_incomplete": [],
+                        "reasons_incomplete_details": "",
+                        "imperfect_but_best_possible": False,
+                        "should_recall": None,
+                    },
+                    doseq=True,
+                )
+            ),
+        )
+        assert not form.is_valid()
+        assert form.errors == {
+            "not_all_mammograms_taken": [
+                'Select "Not all mammograms taken" if a CC or MLO view is missing'
+            ]
+        }
+
+    def test_not_all_mammograms_taken_missing_reason(self):
+        form = AddImageDetailsForm(
+            QueryDict(
+                urlencode(
+                    {
+                        "rmlo_count": 1,
+                        "rcc_count": 1,
+                        "right_eklund_count": 0,
+                        "lmlo_count": 1,
+                        "lcc_count": 0,
+                        "left_eklund_count": 0,
+                        "not_all_mammograms_taken": True,
+                        "reasons_incomplete": [],
+                        "reasons_incomplete_details": "",
+                        "imperfect_but_best_possible": False,
+                        "should_recall": AddImageDetailsForm.RecallChoices.PARTIAL_MAMMOGRAPHY,
+                    },
+                    doseq=True,
+                )
+            ),
+        )
+
+        assert not form.is_valid()
+        assert form.errors == {
+            "reasons_incomplete": [
+                "Select a reason why you could not take all the images"
+            ]
+        }
 
     def _assert_series(
         self, series, expected_view_position, expected_laterality, expected_count
