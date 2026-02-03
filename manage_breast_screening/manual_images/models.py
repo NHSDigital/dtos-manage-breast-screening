c5dd84c631a53e9ef6f6380f897e4c1337cb69d1
Mat Moore <MatMoore@users.noreply.github.com>
2026-02-03 14:11:14 +0000

    add extra fields to Study model
    These capture the mammographer's judgement of whether the image is
    complete, and if not why not.

--- manage_breast_screening/manual_images/models.py
+++ manage_breast_screening/manual_images/models.py
@@ -1,7 +1,44 @@
+from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from manage_breast_screening.core.models import BaseModel
+
+
+class StudyCompleteness(models.TextChoices):
+    """
+    A COMPLETE study is one where at least 1 image is taken
+    for each of the 4 standard views.
+
+    A study is PARTIAL if it is missing images for 1 or more views,
+    but the study is considered done. There is a reason for not
+    taking a full set, that cannot be addressed by recalling
+    the participant for another appointment.
+
+    A study is INCOMPLETE if the full set couldn't be taken,
+    but the reason is temporary, and the participant could
+    be invited back to complete the set.
+    """
+
+    COMPLETE = "COMPLETE", "Complete set of images"
+    PARTIAL = "PARTIAL", "Partial mammography"
+    INCOMPLETE = "INCOMPLETE", "Incomplete set of images"
+
+
+class IncompleteImagesReason(models.TextChoices):
+    CONSENT_WITHDRAWN = "CONSENT_WITHDRAWN", "Consent withdrawn"
+    LANGUAGE_OR_LEARNING_DIFFICULTIES = (
+        "LANGUAGE_OR_LEARNING_DIFFICULTIES",
+        "Language or learning difficulties",
+    )
+    UNABLE_TO_SCAN_TISSUE = "UNABLE_TO_SCAN_TISSUE", "Unable to scan tissue"
+    WHEELCHAIR = "WHEELCHAIR", "Positioning difficulties due to wheelchair"
+    OTHER_MOBILITY = (
+        "OTHER_MOBILITY",
+        "Positioning difficulties for other mobility reasons",
+    )
+    TECHNICAL_ISSUES = "TECHNICAL_ISSUES", "Technical issues"
+    OTHER = "OTHER", "Other"
 
 
 class Study(BaseModel):
@@ -10,6 +47,19 @@ class Study(BaseModel):
     )
 
     additional_details = models.TextField(blank=True, null=False, default="")
+    imperfect_but_best_possible = models.BooleanField(default=False, null=False)
+    completeness = models.CharField(
+        choices=StudyCompleteness, blank=True, null=False, default=""
+    )
+
+    reasons_incomplete = ArrayField(
+        base_field=models.CharField(
+            choices=IncompleteImagesReason, blank=True, null=False, default=""
+        ),
+        default=list,
+        null=False,
+    )
+    reasons_incomplete_details = models.TextField(blank=True, null=False, default="")
 
 
 class Series(BaseModel):
