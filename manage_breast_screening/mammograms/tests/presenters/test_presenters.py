38f836a3880c95e998036b13d316da5921d2bdfd
Mat Moore <MatMoore@users.noreply.github.com>
2026-02-10 14:05:29 +0000

    tests: add missing unit test for secondary nav

---
+++ manage_breast_screening/mammograms/tests/presenters/test_presenters.py
@@ -0,0 +1,49 @@
+from uuid import uuid4
+
+import pytest
+
+from manage_breast_screening.mammograms.presenters import present_secondary_nav
+
+
+class TestSecondaryNav:
+    @pytest.fixture(autouse=True)
+    def pk(self):
+        return uuid4()
+
+    def test_active_appointment(self, pk):
+        assert present_secondary_nav(pk, current_tab="appointment") == [
+            {
+                "current": True,
+                "href": f"/mammograms/{pk}/",
+                "id": "appointment",
+                "text": "Appointment",
+            },
+            {
+                "current": False,
+                "href": f"/mammograms/{pk}/participant/",
+                "id": "participant",
+                "text": "Participant",
+            },
+            {
+                "current": False,
+                "href": "#",
+                "id": "medical_information",
+                "text": "Medical information",
+            },
+            {
+                "current": False,
+                "href": f"/mammograms/{pk}/note/",
+                "id": "note",
+                "text": "Note",
+            },
+        ]
+
+    @pytest.mark.parametrize(
+        "current_tab", ["appointment", "participant", "medical_information", "note"]
+    )
+    def test_current_tab(self, pk, current_tab):
+        tabs = present_secondary_nav(pk, current_tab=current_tab)
+        current = [tab for tab in tabs if tab["current"]]
+
+        assert len(current) == 1
+        assert current[0]["id"] == current_tab
