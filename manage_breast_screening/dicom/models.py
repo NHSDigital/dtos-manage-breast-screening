cca9da143101e97c0d495127b8282d0b2e83a39d
Steve Laing <steve.laing@gmail.com>
2026-02-23 10:09:11 +0000

    Add additional information fields to Study
    Remove a couple of constraints which were incompatible with the dicom standard

--- manage_breast_screening/dicom/models.py
+++ manage_breast_screening/dicom/models.py
@@ -1,9 +1,46 @@
+from django.contrib.postgres.fields import ArrayField
 from django.core.files.storage import storages
 from django.db import models
 
 
 def dicom_storage():
     return storages["dicom"]
+
+
+class StudyCompleteness(models.TextChoices):
+    """
+    A COMPLETE study is one where at least 1 image is taken
+    for each of the 4 standard views.
+
+    A study is PARTIAL if it is missing images for 1 or more views,
+    but the study is considered done. There is a reason for not
+    taking a full set, that cannot be addressed by recalling
+    the participant for another appointment.
+
+    A study is INCOMPLETE if the full set couldn't be taken,
+    but the reason is temporary, and the participant could
+    be invited back to complete the set.
+    """
+
+    COMPLETE = "COMPLETE", "Complete set of images"
+    PARTIAL = "PARTIAL", "Partial mammography"
+    INCOMPLETE = "INCOMPLETE", "Incomplete set of images"
+
+
+class IncompleteImagesReason(models.TextChoices):
+    CONSENT_WITHDRAWN = "CONSENT_WITHDRAWN", "Consent withdrawn"
+    LANGUAGE_OR_LEARNING_DIFFICULTIES = (
+        "LANGUAGE_OR_LEARNING_DIFFICULTIES",
+        "Language or learning difficulties",
+    )
+    UNABLE_TO_SCAN_TISSUE = "UNABLE_TO_SCAN_TISSUE", "Unable to scan tissue"
+    WHEELCHAIR = "WHEELCHAIR", "Positioning difficulties due to wheelchair"
+    OTHER_MOBILITY = (
+        "OTHER_MOBILITY",
+        "Positioning difficulties for other mobility reasons",
+    )
+    TECHNICAL_ISSUES = "TECHNICAL_ISSUES", "Technical issues"
+    OTHER = "OTHER", "Other"
 
 
 class Study(models.Model):
@@ -20,6 +57,21 @@ class Study(models.Model):
     date_and_time = models.DateTimeField(null=True, blank=True)
     description = models.CharField(max_length=255, blank=True)
 
+    additional_details = models.TextField(blank=True, null=False, default="")
+    imperfect_but_best_possible = models.BooleanField(default=False, null=False)
+    completeness = models.CharField(
+        choices=StudyCompleteness, blank=True, null=False, default=""
+    )
+
+    reasons_incomplete = ArrayField(
+        base_field=models.CharField(
+            choices=IncompleteImagesReason, blank=True, null=False, default=""
+        ),
+        default=list,
+        null=False,
+    )
+    reasons_incomplete_details = models.TextField(blank=True, null=False, default="")
+
     def __str__(self):
         return self.study_instance_uid
 
@@ -28,12 +80,6 @@ class Series(models.Model):
     class Meta:
         indexes = [
             models.Index(fields=["series_instance_uid"]),
-        ]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["series_number", "study"],
-                name="unique_series_number_per_study",
-            )
         ]
 
     series_instance_uid = models.CharField(max_length=128, unique=True)
@@ -49,12 +95,6 @@ class Image(models.Model):
     class Meta:
         indexes = [
             models.Index(fields=["sop_instance_uid"]),
-        ]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["instance_number", "series"],
-                name="unique_instance_number_per_series",
-            )
         ]
 
     sop_instance_uid = models.CharField(max_length=128, unique=True)
