cc90ab585b2b7165a62791bc78505dae0f2426aa
Mat Moore <MatMoore@users.noreply.github.com>
2026-03-09 16:46:09 +0000

    allow setting roles in admin

--- manage_breast_screening/users/admin.py
+++ manage_breast_screening/users/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
+from manage_breast_screening.clinics.models import UserAssignment
 from manage_breast_screening.core.admin import admin_site
 
 from .models import User
 
 
+class AssignmentInline(admin.StackedInline):
+    model = UserAssignment
+
+
 class UserAdmin(admin.ModelAdmin):
     exclude = ["password"]
+    inlines = [AssignmentInline]
+    extra = 0
 
 
 admin_site.register(User, UserAdmin)
