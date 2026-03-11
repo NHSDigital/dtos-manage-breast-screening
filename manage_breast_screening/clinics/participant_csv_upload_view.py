119c13df342ae167764f7df7e4e7535cc5ba01ae
Steve Webber <198724792+swebberuk@users.noreply.github.com>
2026-03-11 19:57:53 +0000

    Refactoring of CSV validation

--- manage_breast_screening/clinics/participant_csv_upload_view.py
+++ manage_breast_screening/clinics/participant_csv_upload_view.py
@@ -37,22 +37,12 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
     The clinic must already exist before uploading the CSV. You can create a clinic using the Django admin interface.
     """
 
-    SLOT_DURATION_IN_MINUTES = 15
-    REQUIRED_HEADINGS = [
-        "NHS Number",
-        "Surname",
-        "Forenames",
-        "Date of Birth",
-        "Sex",
-        "Address",
-        "Postcode",
-        "Telephone No.1",
-        "Email Address",
-        "Start Time",
-    ]
-
     template_name = "clinics/participant_csv_upload.jinja"
     form_class = ParticipantCsvUploadForm
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.transformer = ParticipantCsvTransformService()
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_superuser:
@@ -80,14 +70,16 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
             form.add_error("csv_file", "The file could not be read as UTF-8")
             return self.form_invalid(form)
 
-        transformed_rows, csv_errors = self.parse_csv(decoded_file)
+        transformed_rows, csv_errors = self.transformer.transform_csv(decoded_file)
 
         if csv_errors:
             for error in csv_errors:
                 form.add_error(None, error)
             return self.form_invalid(form)
 
-        self.insert_data(transformed_rows)
+        ParticipantCsvInsertService(self.clinic, self.request.user).insert_data(
+            transformed_rows
+        )
 
         messages.success(
             self.request,
@@ -95,22 +87,41 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
         )
         return redirect("clinics:show_clinic", pk=self.clinic.pk)
 
-    def parse_csv(self, decoded_file):
+
+class ParticipantCsvTransformService:
+    def __init__(self, *args, **kwargs):
+        self.field_validators = [
+            ("first_name", "Forenames", self.validate_required),
+            ("last_name", "Surname", self.validate_required),
+            ("gender", "Sex", self.validate_gender),
+            ("nhs_number", "NHS Number", self.validate_nhs_number),
+            ("phone", "Telephone No.1", self.validate_required),
+            ("email", "Email Address", self.validate_required),
+            ("date_of_birth", "Date of Birth", self.validate_date_of_birth),
+            ("address_lines", "Address", self.validate_address),
+            ("postcode", "Postcode", self.validate_required),
+            ("start_time", "Start Time", self.validate_start_time),
+        ]
+        self.required_headings = [
+            csv_column_name for _, csv_column_name, _ in self.field_validators
+        ]
+
+    def transform_csv(self, decoded_file):
         reader = csv.DictReader(io.StringIO(decoded_file))
 
         if len(reader.fieldnames) != len(set(reader.fieldnames)):
-            duplicates = set(
-                [x for x in reader.fieldnames if reader.fieldnames.count(x) > 1]
-            )
-            return (None, [f"Duplicate columns found: {', '.join(duplicates)}"])
+            duplicates = {
+                x for x in reader.fieldnames if reader.fieldnames.count(x) > 1
+            }
+            return None, [f"Duplicate columns found: {', '.join(duplicates)}"]
 
         column_errors = [
             f"Missing column: {heading}"
-            for heading in self.REQUIRED_HEADINGS
+            for heading in self.required_headings
             if heading not in reader.fieldnames
         ]
         if column_errors:
-            return (None, column_errors)
+            return None, column_errors
 
         return self.transform_rows(reader)
 
@@ -119,6 +130,8 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
         Transforms CSV rows into the format needed to insert into the database, while collecting any validation errors.
 
         Returns a tuple containing a list of transformed rows and a list of error messages.
+
+        Rows with errors are excluded from the list of transformed rows.
         """
 
         rows = []
@@ -140,83 +153,88 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
         The errors element is a list of error messages describing any validation issues with the row.
         """
 
+        # If a row does not have enough columns, DictReader fills missing fields with None
         none_keys = [k for k, v in row.items() if v is None]
         if none_keys:
             return (None, [f"Missing columns: {', '.join(none_keys)}"])
 
+        # Strip whitespace from all values
+        row = {k: v.strip() for k, v in row.items()}
+
         errors = []
         output = {}
 
-        first_name = self.get_stripped_value(row, "Forenames")
-        if not first_name:
-            errors.append("Forenames is required")
-        output["first_name"] = first_name
-
-        last_name = self.get_stripped_value(row, "Surname")
-        if not last_name:
-            errors.append("Surname is required")
-        output["last_name"] = last_name
-
-        gender = self.get_stripped_value(row, "Sex")
-        if gender != "F":
-            errors.append(f'Invalid value for "Sex": "{gender}". Only "F" is accepted.')
-        output["gender"] = "Female"
-
-        nhs_number = self.get_stripped_value(row, "NHS Number").replace(" ", "")
-        if not self.is_valid_nhs_number(nhs_number):
-            errors.append(
-                "NHS Number must be 10 digits (spaces are ignored, e.g., 1234567890 or 123 456 7890)"
-            )
-        if Participant.objects.filter(nhs_number=nhs_number).exists():
-            errors.append("NHS Number already exists in the database")
-        output["nhs_number"] = nhs_number
-
-        phone = self.get_stripped_value(row, "Telephone No.1")
-        if not phone:
-            errors.append("Telephone No.1 is required")
-        output["phone"] = phone
-
-        email = self.get_stripped_value(row, "Email Address")
-        if not email:
-            errors.append("Email Address is required")
-        output["email"] = email
-
-        date_of_birth = self.get_stripped_value(row, "Date of Birth")
-        try:
-            output["date_of_birth"] = datetime.strptime(
-                date_of_birth, "%d-%b-%Y"
-            ).date()
-        except (ValueError, TypeError):
-            errors.append(
-                "Date of Birth must be in format DD-MMM-YYYY (e.g., 01-Jan-1980)"
-            )
-
-        address = self.get_stripped_value(row, "Address")
-        address_lines = [line.strip() for line in address.split(",") if line.strip()]
-        if len(address_lines) == 0:
-            errors.append("Address is required")
-        if len(address_lines) > 5:
-            errors.append("Address must have at most 5 lines")
-        output["address_lines"] = address_lines
-
-        postcode = self.get_stripped_value(row, "Postcode")
-        if not postcode:
-            errors.append("Postcode is required")
-        output["postcode"] = postcode
-
-        start_time = self.get_stripped_value(row, "Start Time")
-        try:
-            output["start_time"] = datetime.strptime(start_time, "%H:%M").time()
-        except ValueError:
-            errors.append("Start Time must be in format HH:MM (e.g., 09:30 or 14:45)")
+        for database_column_name, csv_column_name, validator in self.field_validators:
+            value, field_error = validator(row, csv_column_name)
+            if field_error:
+                errors.append(field_error)
+            output[database_column_name] = value
 
         if errors:
             return None, errors
-
         return output, []
 
+    def validate_required(self, row, key):
+        value = row.get(key)
+        if not value:
+            return None, f"{key} is required"
+        return value, None
+
+    def validate_gender(self, row, key):
+        value = row.get(key)
+        if value != "F":
+            return None, f'Invalid value for "{key}": "{value}". Only "F" is accepted.'
+        return "Female", None
+
+    def validate_nhs_number(self, row, key):
+        nhs_number = row.get(key).replace(" ", "")
+        if not self.is_valid_nhs_number(nhs_number):
+            return (
+                None,
+                "NHS Number must be 10 digits (spaces are ignored, e.g., 1234567890 or 123 456 7890)",
+            )
+        if Participant.objects.filter(nhs_number=nhs_number).exists():
+            return None, "NHS Number already exists in the database"
+        return nhs_number, None
+
+    def is_valid_nhs_number(self, nhs_number):
+        return nhs_number.isdigit() and len(nhs_number) == 10
+
+    def validate_date_of_birth(self, row, key):
+        try:
+            return datetime.strptime(row.get(key), "%d-%b-%Y").date(), None
+        except (ValueError, TypeError):
+            return (
+                None,
+                "Date of Birth must be in format DD-MMM-YYYY (e.g., 01-Jan-1980)",
+            )
+
+    def validate_address(self, row, key):
+        address_lines = [
+            line.strip() for line in row.get(key).split(",") if line.strip()
+        ]
+        if len(address_lines) == 0:
+            return None, "Address is required"
+        if len(address_lines) > 5:
+            return None, "Address must have at most 5 lines"
+        return address_lines, None
+
+    def validate_start_time(self, row, key):
+        try:
+            return datetime.strptime(row.get(key), "%H:%M").time(), None
+        except ValueError:
+            return None, "Start Time must be in format HH:MM (e.g., 09:30 or 14:45)"
+
+
+class ParticipantCsvInsertService:
+    SLOT_DURATION_IN_MINUTES = 15
+
+    def __init__(self, clinic, user):
+        self.clinic = clinic
+        self.user = user
+        self.auditor = Auditor(user)
+
     def insert_data(self, transformed_rows):
-        auditor = Auditor.from_request(self.request)
         clinic_date = self.clinic.starts_at.date()
 
         with transaction.atomic():
@@ -231,14 +249,14 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
                     date_of_birth=row["date_of_birth"],
                     risk_level="Routine",
                 )
-                self.save(auditor, participant)
+                self.save_and_audit(participant)
 
                 participant_address = ParticipantAddress(
                     participant=participant,
                     lines=row["address_lines"],
                     postcode=row["postcode"],
                 )
-                self.save(auditor, participant_address)
+                self.save_and_audit(participant_address)
 
                 clinic_slot = ClinicSlot(
                     clinic=self.clinic,
@@ -247,33 +265,26 @@ class ParticipantCsvUploadView(LoginRequiredMixin, FormView):
                     ),
                     duration_in_minutes=self.SLOT_DURATION_IN_MINUTES,
                 )
-                self.save(auditor, clinic_slot)
+                self.save_and_audit(clinic_slot)
 
                 screening_episode = ScreeningEpisode(
                     participant=participant,
                 )
-                self.save(auditor, screening_episode)
+                self.save_and_audit(screening_episode)
 
                 appointment = Appointment(
                     screening_episode=screening_episode,
                     clinic_slot=clinic_slot,
                 )
-                self.save(auditor, appointment)
+                self.save_and_audit(appointment)
 
                 appointment_status = AppointmentStatus(
                     appointment=appointment,
                     name=AppointmentStatusNames.SCHEDULED,
-                    created_by=self.request.user,
+                    created_by=self.user,
                 )
-                self.save(auditor, appointment_status)
+                self.save_and_audit(appointment_status)
 
-    def get_stripped_value(self, row, key):
-        value = row.get(key)
-        return value.strip() if value else ""
-
-    def is_valid_nhs_number(self, nhs_number):
-        return nhs_number.isdigit() and len(nhs_number) == 10
-
-    def save(self, auditor, obj):
+    def save_and_audit(self, obj):
         obj.save()
-        auditor.audit_create(obj)
+        self.auditor.audit_create(obj)
