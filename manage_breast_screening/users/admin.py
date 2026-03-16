b533888149f8560c8d89b985d92fc0abf957146b
Malcolm Baig <git@baig.org>
2026-03-16 11:35:17 +0000

    Use checkboxes for roles in user assignment admin
    Replaces the free-text ArrayField widget with a CheckboxSelectMultiple
    bound to the Role enum choices, preventing invalid values from being
    entered.

--- manage_breast_screening/users/admin.py
+++ manage_breast_screening/users/admin.py
@@ -1,13 +1,29 @@
+from django import forms
 from django.contrib import admin
 
+from manage_breast_screening.auth.models import Role
 from manage_breast_screening.clinics.models import UserAssignment
 from manage_breast_screening.core.admin import admin_site
 
 from .models import User
 
+_ROLE_CHOICES = [(role.value, role.value) for role in Role]
+
+
+class UserAssignmentForm(forms.ModelForm):
+    roles = forms.TypedMultipleChoiceField(
+        choices=_ROLE_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = UserAssignment
+        fields = "__all__"
+
 
 class AssignmentInline(admin.StackedInline):
     model = UserAssignment
+    form = UserAssignmentForm
 
 
 class UserAdmin(admin.ModelAdmin):
