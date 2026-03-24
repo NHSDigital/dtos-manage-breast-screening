813c58206fdfc4084c61389810a6f1cdc917df29
Josiel Souza <josiel.souza@nordcloud.com>
2026-03-24 16:39:24 +0000

    DTOSS-12552: Warm up Jinja2 template cache at worker startup
    Identified during analysis of DTOSS-12504.

--- manage_breast_screening/config/wsgi.py
+++ manage_breast_screening/config/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -16,3 +17,19 @@ os.environ.setdefault(
 )
 
 application = get_wsgi_application()
+
+# Warm up the Jinja2 template cache so the first user request does not pay
+# the PackageLoader traversal cost (~11 s in Azure Container Apps due to
+# slower container filesystem I/O compared to local development).
+#
+# Rendering the base layout template forces Jinja2 to compile and cache
+# all inherited and imported templates (NHS UK Frontend components etc.)
+# that are loaded from the nhsuk_frontend_jinja package via PackageLoader.
+_logger = logging.getLogger(__name__)
+try:
+    from django.template.loader import render_to_string
+
+    render_to_string("layout-app.jinja", {})
+    _logger.info("Jinja2 template cache warmed up")
+except Exception:
+    _logger.warning("Jinja2 template cache warmup failed", exc_info=True)
