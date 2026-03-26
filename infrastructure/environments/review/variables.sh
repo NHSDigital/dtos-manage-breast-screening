2d36a8d76257ebabb34d9c4fbc18793e5b532ea1
Josiel Souza <josiel.souza@nordcloud.com>
2026-03-26 10:46:54 +0000

    Adding the terraform modules branch required to test this solution

--- infrastructure/environments/review/variables.sh
+++ infrastructure/environments/review/variables.sh
@@ -2,6 +2,6 @@ ENV_CONFIG=review
 AZURE_SUBSCRIPTION="Breast Screening - Manage Breast Screening - Dev"
 HUB_SUBSCRIPTION="Digital Screening DToS - DevOps"
 HUB=dev
-TERRAFORM_MODULES_REF=main
+TERRAFORM_MODULES_REF=DTOSS-12524-slack-exception-alert-notifications
 ENABLE_SOFT_DELETE=false
 ADO_MANAGEMENT_POOL=private-pool-dev-uks
