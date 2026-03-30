9a259b202a175014b84c0b669ca5b76e0bdac814
Steve Laing <steve.laing@gmail.com>
2026-03-30 15:56:58 +0100

    Add repeat reasons fields count methods to DICOM Series model

---
+++ manage_breast_screening/dicom/tests/test_models.py
@@ -0,0 +1,77 @@
+import pytest
+
+from manage_breast_screening.dicom.models import Study
+from manage_breast_screening.dicom.tests.factories import (
+    ImageFactory,
+    SeriesFactory,
+    StudyFactory,
+)
+from manage_breast_screening.gateway.tests.factories import GatewayActionFactory
+
+
+@pytest.mark.django_db
+class TestStudy:
+    def test_study_has_series_with_multiple_images(self):
+        series = SeriesFactory.create()
+        ImageFactory.create_batch(2, series=series)
+        study = series.study
+
+        assert study.has_series_with_multiple_images() is True
+
+    def test_study_series_with_multiple_images_only_counts_series_with_more_than_one_image(
+        self,
+    ):
+        series = SeriesFactory.create()
+        ImageFactory.create_batch(2, series=series)
+        ImageFactory.create(series=series)
+        study = series.study
+
+        assert study.series_with_multiple_images().count() == 1
+
+    def test_study_does_not_have_series_with_multiple_images(self):
+        study = StudyFactory.create()
+        ImageFactory.create(series__study=study)
+
+        assert study.has_series_with_multiple_images() is False
+
+    def test_study_for_appointment(self):
+        study = StudyFactory.create()
+        action = GatewayActionFactory.create(id=study.source_message_id)
+
+        assert Study.for_appointment(action.appointment) == study
+
+
+@pytest.mark.django_db
+class TestSeries:
+    def test_series_count_property(self):
+        series = SeriesFactory.create()
+        ImageFactory.create_batch(3, series=series)
+
+        assert series.count == 3
+
+    def test_series_extra_count_property(self):
+        series = SeriesFactory.create()
+        ImageFactory.create(series=series)
+        ImageFactory.create(series=series, implant_present=True)
+
+        assert series.extra_count == 1
+
+
+@pytest.mark.parametrize(
+    "laterality, view_position, expected",
+    [
+        ("R", "MLO", "RMLO"),
+        ("R", "CC", "RCC"),
+        ("L", "MLO", "LMLO"),
+        ("L", "CC", "LCC"),
+        ("R", "", ""),
+        ("", "MLO", ""),
+        ("", "", ""),
+    ],
+)
+@pytest.mark.django_db
+class TestImage:
+    def test_laterality_and_view(self, laterality, view_position, expected):
+        image = ImageFactory.create(laterality=laterality, view_position=view_position)
+
+        assert image.laterality_and_view() == expected
